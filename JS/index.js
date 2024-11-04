f71d5c30dce0071e341c3d00941385ff6c1ad685
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-04 20:55:19 +0500

    let vs var

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+// console.log("1");
+
+// let vs var
+
+// function testVarLet() {
+//   if (true) {
+//     var x = 10; // `var` is function-scoped
+//     let y = 20; // `let` is block-scoped
+//   }
+//   console.log(x); // 10 (Accessible, since x is function-scoped)
+//   console.log(y); // Error (Not accessible, since y is block-scoped)
+// }
+// testVarLet();
