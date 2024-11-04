b2c50fa095714088056c3ccc48d13bb02856052d
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-04 20:55:35 +0500

    let vs var

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,17 @@
 
 // let vs var
 
+// Var
+// The variables that are defined with var statement have function scope.
+// We can declare a variable again even if it has been defined previously in the same scope.
+// Hoisting is allowed with var.
+
+// Let
+
+// The variables that are defined with let statement have block scope.
+// We cannot declare a variable more than once if we defined that previously in the same scope.
+// let is hoisted but inaccessible due to temporal dead zone
+
 // function testVarLet() {
 //   if (true) {
 //     var x = 10; // `var` is function-scoped
