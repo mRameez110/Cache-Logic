78c9ec07362f90ab3c7c50fa492dba467db59152
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-05 21:36:47 +0500

    indexOf Method

--- JS/index.js
+++ JS/index.js
@@ -5,6 +5,13 @@
 // console.log(arrStr[0][2][0][2]); // Getted 'h' from nested Array
 // console.log(typeof arrStr);
 
-let myName = ["M", "Rameez", "G"];
-let joinTest = myName.join("_");
-console.log(joinTest);
+// let myName = ["M", "Rameez", "G"];
+// let joinTest = myName.join("_");
+// console.log(joinTest);
+
+// The indexOf() method returns the first index (position) of a specified value.
+// The indexOf() method returns -1 if the value is not found.
+
+let numArr = [1, 2, 3, 4, 5];
+let indexOfTest = numArr.indexOf(5);
+console.log(indexOfTest);
