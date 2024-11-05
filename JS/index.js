d0ea464f6be869544ce6d4eaf3ea46448c287224
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-05 21:37:44 +0500

    concationation method

--- JS/index.js
+++ JS/index.js
@@ -12,6 +12,15 @@
 // The indexOf() method returns the first index (position) of a specified value.
 // The indexOf() method returns -1 if the value is not found.
 
-let numArr = [1, 2, 3, 4, 5];
-let indexOfTest = numArr.indexOf(5);
-console.log(indexOfTest);
+// let numArr = [1, 2, 3, 4, 5];
+// let indexOfTest = numArr.indexOf(5);
+// console.log(indexOfTest);
+
+// Concatination
+
+// The concat() method concatenates (joins) two or more arrays.
+// The concat() method returns a new array, containing the joined arrays.
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5];
+let concatinationTest = arr1.concat(arr2); // Make 1 combined array
+console.log(concatinationTest);
