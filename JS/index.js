eebee588d7dc6fb7b88cdd026bbed8b0798cdb52
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-05 21:34:45 +0500

    Join(Array Method)

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,10 @@
 // Array(Methods)
 
-let arrStr = [[1, 2, ["Muh"]], "Rameez", "sheikh"];
+// let arrStr = [[1, 2, ["Muh"]], "Rameez", "sheikh"];
 
-console.log(arrStr[0][2][0][2]); // Getted 'h' from nested Array
-console.log(typeof arrStr);
+// console.log(arrStr[0][2][0][2]); // Getted 'h' from nested Array
+// console.log(typeof arrStr);
+
+let myName = ["M", "Rameez", "G"];
+let joinTest = myName.join("_");
+console.log(joinTest);
