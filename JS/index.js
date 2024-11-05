15502134616352ec99d545db136101058df44097
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-05 19:43:30 +0500

    3 methods for extracting a piece of string

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+// String Methods
+// String Methods
+
+// Different Methods to Extracting String Parts
+
+// There are 3 methods for extracting a part of a string:
+// •	slice(start, end)
+// •	substring(start, end)
+// •	substr(start, length)
