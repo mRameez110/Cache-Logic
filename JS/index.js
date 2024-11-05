800a4ffcc6191b58d35c55cd70d609d33d925fae
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-05 19:44:43 +0500

    slice(String Method)

--- JS/index.js
+++ JS/index.js
@@ -7,3 +7,16 @@
 // •	slice(start, end)
 // •	substring(start, end)
 // •	substr(start, length)
+
+// Slice()
+
+// slice() extracts a part of a string and returns the extracted part in a new string.
+// The method takes 2 parameters: start position, and end position (end not included).
+// Can be use with string and also Array.
+
+let myName = "Rameez";
+
+// slice -> to make/ divide string into piece
+
+let sliceTest = myName.slice(2, 4); // starting from index 2 and ending on index 4
+console.log(sliceTest);
