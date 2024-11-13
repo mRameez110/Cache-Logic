973813bc448e53c99b7d313822c54b47e0599c92
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-13 21:21:30 +0500

    Operation evaluation order(BIDMAS)

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,10 @@
-Numbers in JS;
+// Numbers in JS
 
-let a = 5;
-a = 5 ** 3; //  Power Operator
-console.log(a);
+// let a = 5;
+// a = 5 ** 3; //  Power Operator
+// console.log(a);
+
+// order of operation  --> According to BIDMAS
+
+let bidmasTest = 5 * (10 - 3) ** 2;
+console.log(bidmasTest);
