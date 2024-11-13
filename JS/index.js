57dd31f9a1eb869835e73bd3f77e6fc49f3dbcf3
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-13 21:22:33 +0500

    undefined

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,15 @@
-// Numbers in JS
+// i) Numbers in JS
 
 // let a = 5;
 // a = 5 ** 3; //  Power Operator
 // console.log(a);
 
-// order of operation  --> According to BIDMAS
+// ii) Order of operation  --> According to BIDMAS
 
-let bidmasTest = 5 * (10 - 3) ** 2;
-console.log(bidmasTest);
+// let bidmasTest = 5 * (10 - 3) ** 2;
+// console.log(bidmasTest);
+
+// iii)  Undefined
+
+let age;
+console.log(age); // age is not defined, it default value will be undefined
