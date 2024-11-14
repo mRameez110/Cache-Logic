01e613681f9262f7615535ce96f85a7de4f33e28
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-14 21:52:10 +0500

    Implicit type conversion

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+// Type Conversion(Implicit & Explixit)
+
+// Implicit
+// Type conversion by JS Compiler(Behind the scene) is done by Implicit conversion
+
+// e.g;
+
+console.log(5 === "5"); // Comparision done by implicit type conversion
+
+let result = "The number is " + 5; // 5 is converted to string implicity
+console.log(result); // Outputs: "The number is 5"
