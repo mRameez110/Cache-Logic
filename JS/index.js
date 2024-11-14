7255a7d7448661ef3d5cd45337104ee895e38da1
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-14 21:59:11 +0500

    String to Number(Explicit type conversion)

--- JS/index.js
+++ JS/index.js
@@ -5,7 +5,23 @@
 
 // e.g;
 
-console.log(5 === "5"); // Comparision done by implicit type conversion
+// console.log(5 === "5"); // Comparision done by implicit type conversion
 
-let result = "The number is " + 5; // 5 is converted to string implicity
-console.log(result); // Outputs: "The number is 5"
+// let result = "The number is " + 5; // 5 is converted to string implicity
+// console.log(result); // Outputs: "The number is 5"
+
+// Explicit
+
+let score = "100";
+// Score type is string but we want as number
+console.log(typeof score);
+
+// Do type Conversion
+
+score = Number(score); // Convert string score into number score
+console.log(typeof score);
+
+let hel = "hello";
+console.log(typeof hel); // type of hel is string
+hel = Number(hel); //  As we converting Non Number String into number it will return Nan
+console.log(hel);
