89890cc0786b615450499d4d0c4d7b4a4b89f02a
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-18 21:27:23 +0500

    put in folder named Type Conversion

---
+++ JS/6 Type Conversion/index.js
@@ -0,0 +1,34 @@
+// Type Conversion(Implicit & Explixit)
+
+// Implicit
+// Type conversion by JS Compiler(Behind the scene) is done by Implicit conversion
+
+// e.g;
+
+// console.log(5 === "5"); // Comparision done by implicit type conversion
+
+// let result = "The number is " + 5; // 5 is converted to string implicity
+// console.log(result); // Outputs: "The number is 5"
+
+// Explicit
+
+// let score = "100";
+// // Score type is string but we want as number
+// console.log(typeof score);
+
+// // Do type Conversion
+
+// score = Number(score); // Convert string score into number score
+// console.log(typeof score);
+
+// let hel = "hello";
+// console.log(typeof hel); // type of hel is string
+// hel = Number(hel); //  As we converting Non Number String into number it will return Nan
+// console.log(hel);
+
+// -> Number to String
+let num = 50;
+console.log(typeof num); // Its type is Number
+
+num = String(num);
+console.log(typeof num); // Now it will be string
