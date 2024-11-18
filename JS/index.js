2f2fe8731fdaaab003811913a53901bc0a09654c
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-18 21:31:30 +0500

    function argument vs function default parameter

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,22 @@
 // Hoisting
 
-greet1();
+// greet1();
 
-//  function declaration
-function greet1() {
-  console.log("Hello greet 1");
-}
+// //  function declaration
+// function greet1() {
+//   console.log("Hello greet 1");
+// }
 
-// functionExpressionTest(); // This will give error "const not defined"
+// // functionExpressionTest(); // This will give error "const not defined"
 
-//  Functions expression
-const functionExpressionTest = function greet2() {
-  console.log("Hello greet 2");
+// //  Functions expression
+// const functionExpressionTest = function greet2() {
+//   console.log("Hello greet 2");
+// };
+
+// //  Functions expression
+const functionParameterTest = function greet3(name = "Sheikh") {
+  console.log("Hello " + name);
 };
+
+functionParameterTest("MR"); // If I pass Argument it will override the param default value that is 'Sheikh'
