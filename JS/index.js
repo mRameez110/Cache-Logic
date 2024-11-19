8e4926c8d16726ac6a451b98ea4be274bc563685
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-19 21:32:56 +0500

    Object literals and this keyword

---
+++ JS/index.js
@@ -0,0 +1,24 @@
+//   Objects
+
+const obj = {
+  name: "rameez",
+  login: function () {
+    console.log("its login function");
+  },
+
+  logut: function () {
+    console.log("Print login function in logout method", this.login); // this inside object refere to current object
+    console.log("this refer to object", this);
+  },
+
+  logut2: () => {
+    console.log("Print login function in logout arrow function", this.login); // this inside object refere to current obj
+    console.log(
+      "this in arrow function inside object also refer to global object ",
+      this
+    );
+  },
+};
+
+obj.logut2();
+console.log("this refer to object", this); // this independently(outside) refer to global object
