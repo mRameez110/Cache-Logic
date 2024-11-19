e5f539b1d9880b46db413cda1c0010de5df86982
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-19 21:33:59 +0500

    construction function and this keyword

--- JS/index.js
+++ JS/index.js
@@ -1,24 +1,37 @@
 //   Objects
 
-const obj = {
-  name: "rameez",
-  login: function () {
-    console.log("its login function");
-  },
+// const obj = {
+//   name: "rameez",
+//   login: function () {
+//     console.log("its login function");
+//   },
 
-  logut: function () {
-    console.log("Print login function in logout method", this.login); // this inside object refere to current object
-    console.log("this refer to object", this);
-  },
+//   logut: function () {
+//     console.log("Print login function in logout method", this.login); // this inside object refere to current object
+//     console.log("this refer to object", this);
+//   },
 
-  logut2: () => {
-    console.log("Print login function in logout arrow function", this.login); // this inside object refere to current obj
-    console.log(
-      "this in arrow function inside object also refer to global object ",
-      this
-    );
-  },
-};
+//   logut2: () => {
+//     console.log("Print login function in logout arrow function", this.login); // this inside object refere to current obj
+//     console.log(
+//       "this in arrow function inside object also refer to global object ",
+//       this
+//     );
+//   },
+// };
 
-obj.logut2();
-console.log("this refer to object", this); // this independently(outside) refer to global object
+// obj.logut2();
+// console.log("this refer to object", this); // this independently(outside) refer to global object
+
+// .......  this in Constructor Functions
+
+// In constructor functions (functions used to create objects), this refers to the newly created object.
+
+// Example:
+
+function Person(name) {
+  this.name = name;
+}
+
+const rameez = new Person("Rameez");
+console.log(rameez.name); // "Rameez"
