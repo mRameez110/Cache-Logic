958523e9c89b87950e4ef0243cb2e8e99fc0032c
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-21 21:17:38 +0500

    Shallow copy

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,18 @@
 // // 1- Shallow Copy
 
 // // When we copy the reference of non-primitive(reference data types)
+
+// 1- Shallow Copy
+
+let arr1 = [1, 2, 3]; // arrays is reference data type
+// arr1 containg reference of array [1,2,3]
+
+let shallowCopy = arr1; // reference stored in arr1 will be copy in shallowCopy
+// As shallow copy contain same reference as arr1
+// so change in anyone of BroadcastChannel, will effect other one also
+// And this is shallow copy
+
+arr1.push(40); // change in arr1 also effect shallow copy
+
+console.log(arr1);
+console.log(shallowCopy);
