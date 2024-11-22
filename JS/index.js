89d01cf517d01b84e7e0bb995ff1713f4ebd335c
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-22 21:05:39 +0500

    Promise All

--- JS/index.js
+++ JS/index.js
@@ -41,43 +41,83 @@
 
 // Nested Promise(Promise inside promise)
 
-function data1() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("Getting data of function 1.....");
-      resolve("data1 successed");
-    }, 1000);
-  });
-}
+// function data1() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Getting data of function 1.....");
+//       resolve("data1 successed");
+//     }, 1000);
+//   });
+// }
 
-function data2() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("Getting data of function 2.....");
-      resolve("data 2 successed");
-    }, 1000);
-  });
-}
+// function data2() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Getting data of function 2.....");
+//       resolve("data 2 successed");
+//     }, 1000);
+//   });
+// }
 
-function data3() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("Getting data of function 3.....");
-      resolve("data 3 successed");
-    }, 1000);
-  });
-}
+// function data3() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Getting data of function 3.....");
+//       resolve("data 3 successed");
+//     }, 1000);
+//   });
+// }
 
-const f1 = data1();
+// const f1 = data1();
 
-f1.then((resolve) => {
-  console.log(resolve);
-  const f2 = data2();
-  f2.then((resolve) => {
-    console.log(resolve);
-    const f3 = data3();
-    f3.then((resolve) => {
-      console.log(resolve);
-    });
-  });
-});
+// f1.then((resolve) => {
+//   console.log(resolve);
+//   const f2 = data2();
+//   f2.then((resolve) => {
+//     console.log(resolve);
+//     const f3 = data3();
+//     f3.then((resolve) => {
+//       console.log(resolve);
+//     });
+//   });
+// });
+
+// ........ Run promises in Sequence (Promise Chain)
+
+// function asynFunction(dataId) {
+//   return new Promise((res, rej) => {
+//     console.log(`Getting data ${dataId}...`);
+//     setTimeout(() => {
+//       console.log("data", dataId);
+//       res("successed ");
+//     }, 3000);
+//   });
+// }
+
+// asynFunction(1)
+//   .then((res) => {
+//     console.log(res);
+//     return asynFunction(2);
+//   })
+//   .then((res) => {
+//     console.log(res);
+//     return asynFunction(3);
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
+
+//  ..... promise Resolve all
+//  Promise.all will reject as soon as one of the Promises in the array reject.
+
+const pr1 = Promise.resolve("Prom1 Resolved");
+const pr2 = 5;
+const pr3 = new Promise((resolve, reject) =>
+  setTimeout(resolve, 3000, "Good time done")
+);
+
+const allPromises = [pr1, pr2, pr3];
+
+Promise.all(allPromises)
+  .then((value) => console.log(value))
+  .catch((err) => console.log("Geted error is ", err));
