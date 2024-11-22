6c35885f71d5f25b78a31a3ead5fce3a579ca018
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-22 21:01:03 +0500

    Callback

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,21 @@
 // Async JS  - Callbacks, Promises, Async Await
+
+// 1-  ........  Callback hell
+
+function sum(n1, n2) {
+  let sum = n1 + n2;
+  console.log("sum is", sum);
+  sum2(sum, 3);
+}
+
+function sum2(prevSum, n3) {
+  let sum2 = prevSum + n3;
+  console.log("sum is", sum2);
+  sum3(sum2, 4);
+}
+function sum3(prevSum, n4) {
+  let sum3 = prevSum + n4;
+  console.log("sum is", sum3);
+}
+
+sum(1, 2, sum2);
