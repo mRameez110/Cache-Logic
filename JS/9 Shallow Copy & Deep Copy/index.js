3a10dfc2b7bc760996b9c5263f1dc6024cc0e0d0
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-11-22 20:58:55 +0500

    Put in Folder named Shallow and Deep Copy

---
+++ JS/9 Shallow Copy & Deep Copy/index.js
@@ -0,0 +1,45 @@
+// //    ......  Shallow Copy and Deep copy
+
+// // Primitive data types are always copied as deep copy(by value)
+// // 1- Shallow Copy
+
+// // When we copy the reference of non-primitive(reference data types)
+
+// 1- Shallow Copy
+
+// let arr1 = [1, 2, 3]; // arrays is reference data type
+// // arr1 containg reference of array [1,2,3]
+
+// let shallowCopy = arr1; // reference stored in arr1 will be copy in shallowCopy
+// // As shallow copy contain same reference as arr1
+// // so change in anyone of BroadcastChannel, will effect other one also
+// // And this is shallow copy
+
+// arr1.push(40); // change in arr1 also effect shallow copy
+
+// console.log(arr1);
+// console.log(shallowCopy);
+
+// 2 - Deep Copy
+
+// deep copy(Can be by spread operator)
+// A complete independt copy of reference data type
+// change in one not effect other one
+
+// let arr2 = [1, 2, 3];
+// let deepCopy = [...arr2];
+
+// deepCopy.push(99); // change in copied array(deepCopy) will not effect original(arr2) Array
+
+// console.log(arr2);
+// console.log(deepCopy);
+
+// Note:
+// If data is nested then spread operator only copy root level(top-1) as copy by value and nested data as copy by reference
+// in case of nested data spread operator(...) will behave as shallowCopy
+
+const first_person = {
+  name: "Jack",
+  age: 24,
+  lt: {},
+};
