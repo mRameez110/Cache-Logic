15355bdf81a734b5bc34b4178f5f19a74125ba87
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-12-02 21:08:33 +0500

    Commit out code snipt that cause application crash)

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,18 @@
-// Exception Handling
+// // Exception Handling
 
-// Below code stop/crash server if user not found(Run time error)
+// // Below code stop/crash server if user not found(Run time error)
 
-function userFind(id) {
-  const users = { 1: "rameez", 2: "Ashraf" };
+// function userFind(id) {
+//   const users = { 1: "rameez", 2: "Ashraf" };
 
-  let user1 = users[id];
+//   let user1 = users[id];
 
-  console.log("user not found");
-  user1.toUpperCase(); // This will crash server if user undefined(not found)
+//   console.log("user not found");
+//   user1.toUpperCase(); // This will crash server if user undefined(not found)
 
-  console.log(user1);
-}
+//   console.log(user1);
+// }
 
-userFind(3);
+// userFind(3);
 
-console.log("This will not run."); // This will not run because the application crashed
+// console.log("This will not run."); // This will not run because the application crashed
