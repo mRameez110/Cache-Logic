fac40ac587e7e282c1e3dca6608a74e8c7960995
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-12-04 21:06:19 +0500

    creat car class object

---
+++ JS/index.js
@@ -0,0 +1,15 @@
+// Classes in JS
+
+// Creating a class with 2 properties
+
+// constructor -> It run when ever any object of the class creates
+
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+    console.log(this.name, this.year);
+  }
+}
+
+const myCar1 = new Car("wagonR", 2015);
