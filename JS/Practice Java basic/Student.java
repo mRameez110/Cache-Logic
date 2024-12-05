7f066c8f7fe62592e5055f064623dfff92259dae
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-12-05 20:07:53 +0500

    Access static method of Student class

---
+++ JS/Practice Java basic/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+
+    static int age = 5;
+     String name;
+
+    public static void staticTest( ) {
+        System.out.println("I am accessed by using static way ");
+    }
+    
+}
---
+++ JS/Practice Java basic/TestApp.java
@@ -0,0 +1,15 @@
+// Static Method Demo
+  
+public class TestApp {
+
+    public static void main(String args[]) {
+        
+        
+        System.out.println(Student.age); // Acees Static Instance without creating 
+        
+        Student.staticTest();  // Acessing static Method
+
+    }
+    
+}
+
