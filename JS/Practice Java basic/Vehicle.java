e65ce05f62dd11ceb2fd2ba318ead2fb50e645d0
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-12-05 20:21:44 +0500

    Created Vehicle Class

---
+++ JS/Practice Java basic/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle {
+
+    public String VehicleBrand = "Audi";
+
+    public void vehicleSound(){
+       System.out.println(  "\nVehicle horn sound is Honk, honk");
+    }
+    
+}
