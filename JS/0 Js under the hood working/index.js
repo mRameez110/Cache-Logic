50067c74d84271c477a3691cb6b0737b49174329
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-12-09 21:19:20 +0500

    Hoisting of variable

--- JS/0 Js under the hood working/index.js
+++ JS/0 Js under the hood working/index.js
@@ -19,3 +19,17 @@
 // f1();
 
 // Can check it execution flow from source under the dev tool
+
+// Hoisting
+
+console.log(x); // undefined as it created with undefined and also executed with value undefined
+// console.log(a); // ReferenceError: Cannot access 'a' before initialization As let // have block scope and var have global scope
+
+var x = 5;
+var y = 3;
+// let a = 10;
+
+hoistingTest();
+function hoistingTest() {
+  console.log("Hositing testing");
+}
