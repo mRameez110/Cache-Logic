d145ad67d5ba2bb5752d6f5c5f44fb232fb72198
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-12-09 21:18:33 +0500

    Can check it execution flow from source under the dev tool

--- JS/0 Js under the hood working/index.js
+++ JS/0 Js under the hood working/index.js
@@ -2,18 +2,20 @@
 
 // functions will go in call stack(Function Execution Context) and follow LIFO
 
-function f1() {
-  console.log(1);
-  f2();
-}
+// function f1() {
+//   console.log(1);
+//   f2();
+// }
 
-function f2() {
-  console.log(2);
-  f3();
-}
+// function f2() {
+//   console.log(2);
+//   f3();
+// }
 
-function f3() {
-  console.log(3);
-}
+// function f3() {
+//   console.log(3);
+// }
 
-f1();
+// f1();
+
+// Can check it execution flow from source under the dev tool
