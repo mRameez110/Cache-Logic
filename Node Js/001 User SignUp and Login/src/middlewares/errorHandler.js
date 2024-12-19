597e176043cbe4cf86fa50b69fe4bdd2c23584d0
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-12-19 20:53:10 +0500

    two user added hardcoded

---
+++ Node Js/001 User SignUp and Login/src/middlewares/errorHandler.js
@@ -0,0 +1,13 @@
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  const errorMessage = err.message || "XYZ";
+  const statuscode = err.statusCode || 500;
+  console.log("what is error message", errorMessage);
+
+  res.status(statuscode).json({
+    message: errorMessage,
+    ErrorCode: statuscode,
+  });
+};
+
+module.exports = errorHandler;
---
+++ Node Js/001 User SignUp and Login/src/utils/mockUsersData.js
@@ -0,0 +1,14 @@
+const users = [
+  {
+    id: 1,
+    username: "a",
+    email: "a@gmail.com",
+    password: "",
+  },
+  {
+    id: 2,
+    username: "b",
+    email: "@gmail.com",
+    password: "",
+  },
+];
