496b3c2f050f8dfe427d5da46daa5d4bb177f1d7
Rameez <114074269+mRameez110@users.noreply.github.com>
2025-01-01 20:40:49 +0500

    Check request for register

--- Node Js/001 User SignUp and Login/src/middlewares/auth.js
+++ Node Js/001 User SignUp and Login/src/middlewares/auth.js
@@ -1,3 +1,13 @@
 const bcrypt = require("bcrypt");
 const { BadRequestError } = require("../customErrors/customErrorClass");
 const { addNewUser, findUserAgainstEmail } = require("../utils/mockUsersData");
+
+const register = async (req, res, next) => {
+  const { id, username, email, password } = req.body;
+  console.log("Req data check in register midleware", req.body);
+
+  if (!id || !username || !email || !password)
+    return next(new BadRequestError("Please fills all the fields"));
+
+  next();
+};
