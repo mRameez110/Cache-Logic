209daf99df8f86547a97a34430c0c5fcefe3bcf8
mRameez110 <temprary87@gmail.com>
2025-01-23 19:23:31 +0500

    Defined user controller Signup function

--- Node Js/002 Express Folder Structuring/src/controllers/userController.js
+++ Node Js/002 Express Folder Structuring/src/controllers/userController.js
@@ -3,3 +3,17 @@ const userRegServ = require("../services/userRegisterServ");
 const userSigninServ = require("../services/userSignInServ");
 
 
+const signUp = async (req, res) => {
+    const result = await userRegServ(req.body);
+    console.log("The result of new user from services ", result);
+  
+    if (!result)
+      throw new BadRequestError("Something wrong in register new user", 400);
+  
+    const { password: _, ...resultWithoutPassword } = result;
+  
+    res.status(200).send({
+      Request: "Sucessful",
+      New_User: resultWithoutPassword,
+    });
+  };
