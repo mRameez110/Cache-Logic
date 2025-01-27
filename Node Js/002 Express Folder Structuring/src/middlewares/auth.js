25302a55c62683c0a62e04e010a4ca87004ba69f
mRameez110 <temprary87@gmail.com>
2025-01-27 20:44:34 +0500

    register auth

--- Node Js/002 Express Folder Structuring/src/middlewares/auth.js
+++ Node Js/002 Express Folder Structuring/src/middlewares/auth.js
@@ -3,3 +3,15 @@ const { BadRequestError } = require("../customErrors/customErrorClass");
 const { addNewUser, findUserAgainstEmail } = require("../utils/mockUsersData");
 
 
+const register = async (req, res, next) => {
+    const { id, username, email, password } = req.body;
+    console.log("Req data check in register midleware", req.body);
+  
+    if (findUserAgainstEmail(email)) {
+      return next(new BadRequestError("User already exists", 409));
+    }
+    if (!id || !username || !email || !password)
+      return next(new BadRequestError("Please fills all the fields"));
+    next();
+  };
+
