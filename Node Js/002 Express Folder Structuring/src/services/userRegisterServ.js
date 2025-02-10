fae976082bb63d1a8353d1670da68227fc14deeb
mRameez110 <temprary87@gmail.com>
2025-02-10 21:40:59 +0500

    user register service function

--- Node Js/002 Express Folder Structuring/src/services/userRegisterServ.js
+++ Node Js/002 Express Folder Structuring/src/services/userRegisterServ.js
@@ -1,2 +1,12 @@
 const { addNewUser } = require("../utils/mockUsersData");
 const bcrypt = require("bcrypt");
+
+const userRegister = async (dataObject) => {
+  const { id, username, email, password } = dataObject;
+
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password.toString(), saltRounds);
+
+  const newUser = addNewUser(id, username, email, hashedPassword);
+  return newUser;
+};
