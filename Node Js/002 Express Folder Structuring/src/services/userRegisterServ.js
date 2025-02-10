5fcf08fe0018340451c9a649cc4f8d11117711e3
mRameez110 <temprary87@gmail.com>
2025-02-10 21:41:21 +0500

    inspect using consoles

--- Node Js/002 Express Folder Structuring/src/services/userRegisterServ.js
+++ Node Js/002 Express Folder Structuring/src/services/userRegisterServ.js
@@ -4,9 +4,12 @@ const bcrypt = require("bcrypt");
 const userRegister = async (dataObject) => {
   const { id, username, email, password } = dataObject;
 
+  console.log("The data Object data in user Register Services is ", dataObject);
+
   const saltRounds = 10;
   const hashedPassword = await bcrypt.hash(password.toString(), saltRounds);
 
   const newUser = addNewUser(id, username, email, hashedPassword);
+  console.log("new user created in user Register services ", newUser);
   return newUser;
 };
