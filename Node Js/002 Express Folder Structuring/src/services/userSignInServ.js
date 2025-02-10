25e625629414c63efecd4e475198de5ff5c75c74
mRameez110 <temprary87@gmail.com>
2025-02-10 21:44:00 +0500

    signInService function

--- Node Js/002 Express Folder Structuring/src/services/userSignInServ.js
+++ Node Js/002 Express Folder Structuring/src/services/userSignInServ.js
@@ -1,2 +1,15 @@
 const { findUserAgainstEmail } = require("../utils/mockUsersData");
 const jwt = require("jsonwebtoken");
+
+const userSignin = async (reqData) => {
+  const { email, password } = reqData;
+
+  const loginUser = findUserAgainstEmail(email);
+  const { password: _, ...sendUserWithoutPassword } = loginUser;
+
+  const token = jwt.sign({ email: email }, process.env.secret);
+
+  return { user: sendUserWithoutPassword, token };
+};
+
+module.exports = userSignin;
