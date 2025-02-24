5cb6b24afd21015351e46976cdc5fd7a0a7020e3
mRameez110 <temprary87@gmail.com>
2025-02-24 21:38:34 +0500

    Setup basic of app.js

---
+++ Node Js/003 user Authentication with proper folder Structuring/src/app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const { default: helmet } = require("helmet");
+const cors = require("cors");
+const compression = require("compression");
+
+const connectDB = require("./utils/helpers/db");
+
+const app = express();
+
+connectDB();
+
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 3001;
+
+app.use("/user", userRoute);
+
+app.all("*", (req, res) => {
+	throw new RouteNotFoundError();
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log("Server started on PORT ", PORT));
