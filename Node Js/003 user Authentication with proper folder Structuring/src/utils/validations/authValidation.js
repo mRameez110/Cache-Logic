2c3380cc8c1acec3eee6bbe6b5dd2a0f6c9710dc
mRameez110 <temprary87@gmail.com>
2025-02-25 21:09:17 +0500

    defined validation for Signin

--- Node Js/003 user Authentication with proper folder Structuring/src/utils/validations/authValidation.js
+++ Node Js/003 user Authentication with proper folder Structuring/src/utils/validations/authValidation.js
@@ -1,13 +1,21 @@
 const joi = require("joi");
 
 const signUpSchema = joi
-	.object({
-		username: joi.string().min(3).max(50).trim().required(),
-		email: joi.string().email().min(1).max(50).trim().required(),
-		password: joi.string().min(1).required(),
-	})
-	.options({ abortEarly: true });
+  .object({
+    username: joi.string().min(3).max(50).trim().required(),
+    email: joi.string().email().min(1).max(50).trim().required(),
+    password: joi.string().min(1).required(),
+  })
+  .options({ abortEarly: true });
+
+const signInSchema = joi
+  .object({
+    email: joi.string().email().min(1).max(50).required(),
+    password: joi.string().min(1).required(),
+  })
+  .options({ abortEarly: true });
 
 module.exports = {
-	signUpSchema,
+  signUpSchema,
+  signInSchema,
 };
