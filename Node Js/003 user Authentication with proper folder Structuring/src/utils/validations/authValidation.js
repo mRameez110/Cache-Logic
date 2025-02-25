2c558c98f47f9d50a962a50f174856538fd9a514
mRameez110 <temprary87@gmail.com>
2025-02-25 21:08:59 +0500

    defined validation for Signup

---
+++ Node Js/003 user Authentication with proper folder Structuring/src/utils/validations/authValidation.js
@@ -0,0 +1,13 @@
+const joi = require("joi");
+
+const signUpSchema = joi
+	.object({
+		username: joi.string().min(3).max(50).trim().required(),
+		email: joi.string().email().min(1).max(50).trim().required(),
+		password: joi.string().min(1).required(),
+	})
+	.options({ abortEarly: true });
+
+module.exports = {
+	signUpSchema,
+};
