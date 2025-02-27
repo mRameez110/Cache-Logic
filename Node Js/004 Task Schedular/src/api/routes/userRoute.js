243b16e12850cd1f989c76e9472d85ee0f77edf0
mRameez110 <temprary87@gmail.com>
2025-02-27 20:58:11 +0500

    routes and their handlers defined for user routes

---
+++ Node Js/004 Task Schedular/src/api/routes/userRoute.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { createUser } = require("../controllers/userController");
+
+const router = express.Router();
+
+router.get("/getUsers", getUsers);
+router.post("/createUser", createUser);
+router.get("/user/:username", getUserByUserName);
+
+module.exports = router;
