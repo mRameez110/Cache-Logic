cd872af31a8ac0e55eb970c3b53f25817a54196c
mRameez110 <temprary87@gmail.com>
2025-02-27 21:00:45 +0500

    defined createUser

---
+++ Node Js/004 Task Schedular/src/api/controllers/userController.js
@@ -0,0 +1,17 @@
+
+const createUser = async (req, res, next) => {
+  try {
+    validation(req.body, registerValidationSchema);
+    const result = await registerService(req.body);
+
+    res.status(200).json({
+      status: true,
+      message: "User created Successfully",
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { createUser };
