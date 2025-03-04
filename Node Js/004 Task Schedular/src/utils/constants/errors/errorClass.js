816c00dcddefe3f96a04a41603b6d769219e6c01
mRameez110 <temprary87@gmail.com>
2025-03-04 16:55:01 +0500

    defined AppError extended by builtin Error Class

---
+++ Node Js/004 Task Schedular/src/utils/constants/errors/errorClass.js
@@ -0,0 +1,11 @@
+class AppError extends Error {
+	constructor(message = "Something is wrong", errorCode = 500) {
+		super();
+		this.message = message;
+		this.errorCode = errorCode;
+	}
+}
+
+module.exports = {
+	AppError,
+};
