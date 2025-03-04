f9eee6649432e16cad29a3f38dcba8a7c1c75357
mRameez110 <temprary87@gmail.com>
2025-03-04 16:56:58 +0500

    defined bad request error class

--- Node Js/004 Task Schedular/src/utils/constants/errors/errorClass.js
+++ Node Js/004 Task Schedular/src/utils/constants/errors/errorClass.js
@@ -1,26 +1,31 @@
-
 class AppError extends Error {
-  constructor(message = "Something is wrong", errorCode = 500) {
-    super();
-    this.message = message;
-    this.errorCode = errorCode;
-  }
+	constructor(message = "Something is wrong", errorCode = 500) {
+		super();
+		this.message = message;
+		this.errorCode = errorCode;
+	}
 }
 
 class RouteNotFoundError extends AppError {
-  constructor(message = "Route does't exist", errorCode = 404) {
-    super(message, errorCode);
-  }
+	constructor(message = "Route does't exist", errorCode = 404) {
+		super(message, errorCode);
+	}
 }
 class UserAlreadyExistError extends AppError {
-  constructor(message = "User Already Exist", errorCode = 403) {
-    super(message, errorCode);
-  }
+	constructor(message = "User Already Exist", errorCode = 403) {
+		super(message, errorCode);
+	}
 }
 
+class BadRequestError extends AppError {
+	constructor(message = "Bad Request", errorCode = 400) {
+		super(message, errorCode);
+	}
+}
 
 module.exports = {
-  AppError,
-  RouteNotFoundError,
-  UserAlreadyExistError,
+	AppError,
+	RouteNotFoundError,
+	UserAlreadyExistError,
+	BadRequestError,
 };
