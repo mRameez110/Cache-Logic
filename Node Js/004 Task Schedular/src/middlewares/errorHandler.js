88cf9e1b5e6449bcd7b1748aef80a6fa78240684
mRameez110 <temprary87@gmail.com>
2025-03-04 16:47:34 +0500

    fix errorHandler Midleware

--- Node Js/004 Task Schedular/src/middlewares/errorHandler.js
+++ Node Js/004 Task Schedular/src/middlewares/errorHandler.js
@@ -1,14 +1,14 @@
 const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
+	console.error(err.stack);
 
-  const errorMessage = err.message || "Something Bad";
-  const errorCode = err.errorCode || 500;
+	const errorMessage = err.message || "Something Bad";
+	const errorCode = err.errorCode || 500;
 
-  res.status(errorCode).json({
-    status: false,
-    message: errorMessage,
-    data: null,
-  });
+	res.status(errorCode).json({
+		status: false,
+		message: errorMessage,
+		data: null,
+	});
 };
 
 module.exports = errorHandler;
