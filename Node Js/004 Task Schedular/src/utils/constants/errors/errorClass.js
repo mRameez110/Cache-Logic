bba3219eed90d20ee3776b4db6952013183dc20e
mRameez110 <temprary87@gmail.com>
2025-03-04 16:56:13 +0500

    defined route not defined error class

--- Node Js/004 Task Schedular/src/utils/constants/errors/errorClass.js
+++ Node Js/004 Task Schedular/src/utils/constants/errors/errorClass.js
@@ -1,11 +1,19 @@
+
 class AppError extends Error {
-	constructor(message = "Something is wrong", errorCode = 500) {
-		super();
-		this.message = message;
-		this.errorCode = errorCode;
-	}
+  constructor(message = "Something is wrong", errorCode = 500) {
+    super();
+    this.message = message;
+    this.errorCode = errorCode;
+  }
+}
+
+class RouteNotFoundError extends AppError {
+  constructor(message = "Route does't exist", errorCode = 404) {
+    super(message, errorCode);
+  }
 }
 
 module.exports = {
-	AppError,
+  AppError,
+  RouteNotFoundError,
 };
