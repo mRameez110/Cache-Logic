57e37b78e86cec7f6a4b6ab4b5891dad64a425ad
mRameez110 <temprary87@gmail.com>
2025-03-05 16:40:25 +0500

    trim username and email/register validation schema

--- Node Js/004 Task Schedular/src/utils/validations/validation.js
+++ Node Js/004 Task Schedular/src/utils/validations/validation.js
@@ -1,6 +1,6 @@
 const registerValidationSchema = joi
   .object({
-    username: joi.string().min(3).max(50).required(),
-    email: joi.string().email().min(1).max(50).required(),
+    username: joi.string().min(3).max(50).trim().required(),
+    email: joi.string().email().min(1).max(50).trim().required(),
     password: joi.string().min(1).required(),
   })
