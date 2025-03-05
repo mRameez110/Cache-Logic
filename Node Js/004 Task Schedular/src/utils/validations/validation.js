85b0d142741c8b1ff61588daba207ef47441bb6e
mRameez110 <temprary87@gmail.com>
2025-03-05 16:46:31 +0500

    task create validation schema/assignedTo also required field

--- Node Js/004 Task Schedular/src/utils/validations/validation.js
+++ Node Js/004 Task Schedular/src/utils/validations/validation.js
@@ -1,15 +1,16 @@
 const registerValidationSchema = joi
-  .object({
-    username: joi.string().min(3).max(50).trim().required(),
-    email: joi.string().email().min(1).max(50).trim().required(),
-    password: joi.string().min(1).required(),
-  })
-  .options({ abortEarly: true });
+	.object({
+		username: joi.string().min(3).max(50).trim().required(),
+		email: joi.string().email().min(1).max(50).trim().required(),
+		password: joi.string().min(1).required(),
+	})
+	.options({ abortEarly: true });
 
-  const taskCreateValidationSchema = joi
-  .object({
-    title: joi.string().min(5).max(15).trim().required(),
-    description: joi.string().max(50).trim(),
-    createdBy: joi.string().min(2).max(10).trim().required(),
-  })
-  .options({ abortEarly: true });
+const taskCreateValidationSchema = joi
+	.object({
+		title: joi.string().min(5).max(15).trim().required(),
+		description: joi.string().max(50).trim(),
+		createdBy: joi.string().min(2).max(10).trim().required(),
+		assignedTo: joi.string().min(2).max(10).trim().required(),
+	})
+	.options({ abortEarly: true });
