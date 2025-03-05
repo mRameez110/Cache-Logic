cd612910102539faddf5acd2f115beefed74b79a
mRameez110 <temprary87@gmail.com>
2025-03-05 16:42:48 +0500

    task create validation schema

--- Node Js/004 Task Schedular/src/utils/validations/validation.js
+++ Node Js/004 Task Schedular/src/utils/validations/validation.js
@@ -1,7 +1,13 @@
 const registerValidationSchema = joi
-  .object({
-    username: joi.string().min(3).max(50).trim().required(),
-    email: joi.string().email().min(1).max(50).trim().required(),
-    password: joi.string().min(1).required(),
-  })
-  .options({ abortEarly: true });
+	.object({
+		username: joi.string().min(3).max(50).trim().required(),
+		email: joi.string().email().min(1).max(50).trim().required(),
+		password: joi.string().min(1).required(),
+	})
+	.options({ abortEarly: true });
+
+const taskCreateValidationSchema = joi.object({
+	title: joi.string().min(5).max(15),
+	description: joi.string().max(50),
+	createdBy: joi.string().min(2).max(10).required(),
+});
