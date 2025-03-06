0c1d406677eead2bfc71a89b7bbed3aa1fb4a179
mRameez110 <temprary87@gmail.com>
2025-03-06 16:55:51 +0500

    called db connection function

--- Node Js/004 Task Schedular + mail/src/app.js
+++ Node Js/004 Task Schedular + mail/src/app.js
@@ -6,14 +6,14 @@ const compression = require("compression");
 const connectDB = require("./utils/helper/db/connection");
 const app = express();
 
-
 app.use(helmet());
 app.use(cors());
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT || 3001;
+connectDB();
 
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log("Server started on PORT ", PORT));
