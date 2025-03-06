5a7dd1708f7e61699e30dae601544e3ae6042dbf
mRameez110 <temprary87@gmail.com>
2025-03-06 16:50:49 +0500

    setted basic code for main file app.js

---
+++ Node Js/004 Task Schedular + mail/src/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const { default: helmet } = require("helmet");
+const cors = require("cors");
+const compression = require("compression");
+const app = express();
+
+
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 3001;
+
+
+app.listen(PORT, () => console.log("Server started on PORT ", PORT));
