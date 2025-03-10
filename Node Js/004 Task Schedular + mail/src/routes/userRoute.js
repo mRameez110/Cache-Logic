a59e27c2a9196605f574dcfd464c82347215039d
mRameez110 <temprary87@gmail.com>
2025-03-10 16:44:40 +0500

    defined route for get all users

--- Node Js/004 Task Schedular + mail/src/routes/userRoute.js
+++ Node Js/004 Task Schedular + mail/src/routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createUser } = require("../controllers/userController");
+const { createUser, getAllUsers } = require("../controllers/userController");
 
 const router = express.Router();
 
+router.get("/getUsers", getAllUsers);
 router.post("/createUser", createUser);
 
 module.exports = router;
