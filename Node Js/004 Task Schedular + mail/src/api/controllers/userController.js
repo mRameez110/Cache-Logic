a0cba1263adca7c64827fbba1b80dd52700adcbc
mRameez110 <temprary87@gmail.com>
2025-03-10 16:51:57 +0500

    before call register service validate the request using validate function call

--- Node Js/004 Task Schedular + mail/src/api/controllers/userController.js
+++ Node Js/004 Task Schedular + mail/src/api/controllers/userController.js
@@ -2,6 +2,7 @@ const { registerService } = require("../../services/userAuthService");
 
 const createUser = async (req, res, next) => {
 	try {
+    validation(req.body, registerValidationSchema);
 		const result = await registerService(req.body);
 
 		res.status(200).json({
