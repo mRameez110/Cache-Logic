eb27f4564332c3cc87330b2d50dd06f5a03d77bd
mRameez110 <temprary87@gmail.com>
2025-03-11 16:48:41 +0500

    replace anonumus inline function with createTask controller function

--- Node Js/004 Task Schedular + mail/src/api/routes/taskRoute.js
+++ Node Js/004 Task Schedular + mail/src/api/routes/taskRoute.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const { getAllTasks, createTask, } = require("../controllers/taskController");
+const { getAllTasks, createTask } = require("../controllers/taskController");
 
 const router = express.Router();
 
 router.get("/allTasks", getAllTasks);
 
-router.post("/createTask", (req, res) => {
-	res.send("Route for task create");
-});
+router.post("/createTask", createTask);
