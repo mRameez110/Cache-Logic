7cd9139f15f95d76a42a60c60b5fd84a031a1e2e
mRameez110 <temprary87@gmail.com>
2025-03-12 16:46:17 +0500

    defining createtask controller function

--- Node Js/004 Task Schedular + mail/src/api/controllers/taskController.js
+++ Node Js/004 Task Schedular + mail/src/api/controllers/taskController.js
@@ -1,4 +1,26 @@
-const {
-  createTaskService,
-} = require("../../services/taskService");
+const { createTaskService } = require("../../services/taskService");
 
+const createTask = async (req, res, next) => {
+	try {
+		const newTask = await createTaskService(req.body);
+
+		const { title, description, createdBy, assignedTo } = newTask;
+
+		console.log(
+			"new task created detail is",
+			title,
+			description,
+			createdBy,
+			assignedTo
+		);
+
+		const assignedUser = await userModel.findOne({
+			username: assignedTo,
+		});
+		console.log("Task assigned to details ", assignedUser);
+		const receiverMail = assignedUser.email;
+
+			} catch (err) {
+		next(err);
+	}
+};
