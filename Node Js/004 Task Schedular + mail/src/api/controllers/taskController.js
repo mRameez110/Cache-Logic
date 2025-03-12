8a7d8d5531d712645d25aed26437ed7b7ba20dbc
mRameez110 <temprary87@gmail.com>
2025-03-12 16:52:31 +0500

    call getTaskService function

--- Node Js/004 Task Schedular + mail/src/api/controllers/taskController.js
+++ Node Js/004 Task Schedular + mail/src/api/controllers/taskController.js
@@ -64,6 +64,7 @@ const getAllTasks = async (req, res, next) => {
 
 const getTaskByUsername = async (req, res, next) => {
 	try {
+		const task = await getTaskService(req);
 		res.status(200).send({
 			status: true,
 			message: "Task details against user name is below",
