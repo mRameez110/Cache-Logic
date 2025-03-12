5ae39a38449bfaff7357b1d76a49ef33a8b5d15d
mRameez110 <temprary87@gmail.com>
2025-03-12 16:48:36 +0500

    handle email promise using than and catch

--- Node Js/004 Task Schedular + mail/src/api/controllers/taskController.js
+++ Node Js/004 Task Schedular + mail/src/api/controllers/taskController.js
@@ -20,14 +20,26 @@ const createTask = async (req, res, next) => {
 		});
 		console.log("Task assigned to details ", assignedUser);
 		const receiverMail = assignedUser.email;
-		sendEmail(
-			receiverMail,
-			"Task Notification",
-			title,
-			description,
-			createdBy,
-			assignedTo
-		);
+    sendEmail(
+      receiverMail,
+      "Task Notification",
+      title,
+      description,
+      createdBy,
+      assignedTo
+    )
+      .then((response) => {
+        console.log("Email sended successfully", response);
+        res.status(201).json({
+          status: true,
+          message: "Task created successfully",
+          task: newTask,
+        });
+      })
+      .catch((err) => {
+        console.log("Error in sending Email to user or client", err);
+        next(err);
+      });
 	} catch (err) {
 		next(err);
 	}
