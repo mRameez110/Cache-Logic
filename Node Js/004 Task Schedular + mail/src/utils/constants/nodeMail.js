b74226431905a5d1a440abdebef45b2230c25b1a
mRameez110 <temprary87@gmail.com>
2025-03-13 16:50:45 +0500

    transported

--- Node Js/004 Task Schedular + mail/src/utils/constants/nodeMail.js
+++ Node Js/004 Task Schedular + mail/src/utils/constants/nodeMail.js
@@ -1,2 +1,10 @@
 const nodemailer = require("nodemailer");
 const { MailError } = require("../constants/errors/errorClass");
+
+const transporter = nodemailer.createTransport({
+	service: "gmail",
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASS,
+	},
+});
