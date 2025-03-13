2a1bbd9f728d39c712250e0ea24bf4483008aca8
mRameez110 <temprary87@gmail.com>
2025-03-13 16:55:27 +0500

    exported sendEmail

--- Node Js/004 Task Schedular + mail/src/utils/constants/nodeMail.js
+++ Node Js/004 Task Schedular + mail/src/utils/constants/nodeMail.js
@@ -9,33 +9,35 @@ const transporter = nodemailer.createTransport({
 	},
 });
 
-
 const sendEmail = async (
-  to,
-  subject,
-  title,
-  description,
-  createdBy,
-  assignedTo
+	to,
+	subject,
+	title,
+	description,
+	createdBy,
+	assignedTo
 ) => {
-  console.log(
-    "Mail body data ",
-    to,
-    subject,
-    title,
-    description,
-    createdBy,
-    assignedTo
-  );
-  const from = transporter.options.auth.user;
-  console.log("Transporter check ", from);
+	console.log(
+		"Mail body data ",
+		to,
+		subject,
+		title,
+		description,
+		createdBy,
+		assignedTo
+	);
+	const from = transporter.options.auth.user;
+	console.log("Transporter check ", from);
 
-  const mailResponse = await transporter.sendMail({
-    from,
-    to,
-    subject: "Task Notification",
-    html: `h1>${title}</h1><p>${description}</p><p>Created By: ${createdBy}</p><p>Assigned To: ${assignedTo}</p>`,
-  });
-  console.log("Mail Response is ", mailResponse);
-  if (!mailResponse) throw new MailError();
-  return mailResponse;
+	const mailResponse = await transporter.sendMail({
+		from,
+		to,
+		subject: "Task Notification",
+		html: `h1>${title}</h1><p>${description}</p><p>Created By: ${createdBy}</p><p>Assigned To: ${assignedTo}</p>`,
+	});
+	console.log("Mail Response is ", mailResponse);
+	if (!mailResponse) throw new MailError();
+	return mailResponse;
+};
+
+module.exports = { sendEmail };
