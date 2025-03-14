3717296a3716c347aa56b35c76793e945eeebeb7
Rameez <114074269+mRameez110@users.noreply.github.com>
2025-03-14 22:45:51 +0500

    task create validation schme/ defined min max for createdBy

--- Node Js/004 Task Schedular + mail/src/utils/validations/validations.js
+++ Node Js/004 Task Schedular + mail/src/utils/validations/validations.js
@@ -10,7 +10,7 @@ const taskCreateValidationSchema = joi
   .object({
     title: joi.string().min(5).max(15).trim().required(),
     description: joi.string().max(50).trim(),
-    createdBy: joi.string().trim().required(),
+    createdBy: joi.string().min(2).max(10).trim().required(),
     assignedTo: joi.string().trim().required(),
   })
   .options({ abortEarly: true });
