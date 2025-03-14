94104b11bed5ba6b53dca112f61947b0044e9e3b
Rameez <114074269+mRameez110@users.noreply.github.com>
2025-03-14 22:39:30 +0500

    register validation schema /abortEarly:true

--- Node Js/004 Task Schedular + mail/src/utils/validations/validations.js
+++ Node Js/004 Task Schedular + mail/src/utils/validations/validations.js
@@ -1,5 +1,7 @@
-const registerValidationSchema = joi.object({
-  username: joi.string().min(3).max(50).trim().required(),
-  email: joi.string().email().min(1).max(50).trim().required(),
-  password: joi.string().min(1).required(),
-});
+const registerValidationSchema = joi
+  .object({
+    username: joi.string.trim().required(),
+    email: joi.string().email().trim().required(),
+    password: joi.string().required(),
+  })
+  .options({ abortEarly: true });
