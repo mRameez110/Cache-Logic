d5440d1f33e1df0c79425ded3fa94ebc05ab4ef2
mRameez110 <temprary87@gmail.com>
2025-03-17 16:20:14 +0500

    modify styling of mail template

--- Node Js/004 Task Schedular + mail/src/utils/helper/mailTemplate.js
+++ Node Js/004 Task Schedular + mail/src/utils/helper/mailTemplate.js
@@ -1,6 +1,6 @@
 const taskMailTemplate = (title, description, createdBy, assignedTo) =>
 	`
-<div style="font-family: Arial">
+<div style="font-family: Arial sans-serif;">
        <h2 style="color: green;">Task Notification</h2>
      <h4><strong>Task Title:</strong> ${title}</h4>
      <p><strong>Task Description:</strong> ${description}</p>
