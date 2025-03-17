e8a9090a867e1c8788319ea28d541e4dcaf363f3
mRameez110 <temprary87@gmail.com>
2025-03-17 16:18:19 +0500

    task notification

--- Node Js/004 Task Schedular + mail/src/utils/helper/mailTemplate.js
+++ Node Js/004 Task Schedular + mail/src/utils/helper/mailTemplate.js
@@ -1,8 +1,7 @@
-const taskMailTemplate = (title, description, createdBy, assignedTo) => 
-  
-  `
+const taskMailTemplate = (title, description, createdBy, assignedTo) =>
+	`
 <div>
-      
+       <h2 style="color: green;">Task Notification</h2>
      <h4><strong>Task Title:</strong> ${title}</h4>
      <p><strong>Task Description:</strong> ${description}</p>
       <p><strong>Task Created By:</strong> ${createdBy}</p>
