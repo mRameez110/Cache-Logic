e724adae062e475b811dc50b7ede743f1323ca50
mRameez110 <temprary87@gmail.com>
2025-03-21 17:12:34 +0500

    Defined structure for createTaskService function

--- Node Js/004 Task Schedular + mail/src/services/taskService.js
+++ Node Js/004 Task Schedular + mail/src/services/taskService.js
@@ -1,3 +1,7 @@
 const taskModel = require("../models/taskModel");
 const userModel = require("../models/userModel");
 const { BadRequestError } = require("../utils/constants/errors/errorClass");
+
+const createTaskService = async (dataObject) => {
+  const { title, description, createdBy, assignedTo } = dataObject;
+};
