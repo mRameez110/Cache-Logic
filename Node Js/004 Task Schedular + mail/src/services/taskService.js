e00db48b419f97e515defa785e80286115673c31
mRameez110 <temprary87@gmail.com>
2025-03-21 17:12:46 +0500

    Added check for task creator (createdBy) in createTaskService

--- Node Js/004 Task Schedular + mail/src/services/taskService.js
+++ Node Js/004 Task Schedular + mail/src/services/taskService.js
@@ -5,3 +5,12 @@ const { BadRequestError } = require("../utils/constants/errors/errorClass");
 const createTaskService = async (dataObject) => {
   const { title, description, createdBy, assignedTo } = dataObject;
 };
+
+
+const findTaskCrater = await userModel.findOne({ username: createdBy });
+console.log("Task crater finding ", findTaskCrater);
+if (!findTaskCrater)
+  throw new BadRequestError(
+    "Your are not register. first register to create task",
+    400
+  );
