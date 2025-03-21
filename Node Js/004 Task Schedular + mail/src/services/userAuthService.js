8c944fc7741f7bcf80745c7ae2d0cfc1ed615c70
mRameez110 <temprary87@gmail.com>
2025-03-21 17:06:18 +0500

    Imported bcryptjs, userModel, and error classes

---
+++ Node Js/004 Task Schedular + mail/src/services/userAuthService.js
@@ -0,0 +1,6 @@
+const bcrypt = require("bcryptjs");
+const userModel = require("../models/userModel");
+const {
+  UserAlreadyExistError,
+  BadRequestError,
+} = require("../utils/constants/errors/errorClass");
