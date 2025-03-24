852938c4db27847d9c82c13521eb825e138541a8
mRameez110 <temprary87@gmail.com>
2025-03-24 17:19:08 +0500

    Add taskService.js with proper imports and exports

--- Node Js/004 Task Schedular + mail/src/services/taskService.js
+++ Node Js/004 Task Schedular + mail/src/services/taskService.js
@@ -1,22 +1,31 @@
+// Import the necessary models and error utility classes
+const taskModel = require("../models/taskModel"); // Task model for task-related operations
+const userModel = require("../models/userModel"); // User model to check for valid users
+const { BadRequestError } = require("../utils/constants/errors/errorClass"); // Custom error class
+
+// Service to create a task
 const createTaskService = async (dataObject) => {
 	const { title, description, createdBy, assignedTo } = dataObject;
 
+	// Check if the user creating the task exists
 	const findTaskCrater = await userModel.findOne({ username: createdBy });
 	if (!findTaskCrater)
 		throw new BadRequestError(
-			"Your are not register. first register to create task",
+			"You are not registered. First, register to create a task",
 			400
 		);
 
+	// Check if the assigned user exists
 	const findTaskAssignedUser = await userModel.findOne({
 		username: assignedTo,
 	});
 	if (!findTaskAssignedUser)
 		throw new BadRequestError(
-			"Assigned user name does't exist. Username must exist to assign task",
+			"Assigned username doesn't exist. Username must exist to assign a task",
 			400
 		);
 
+	// Create and save the new task
 	const newTask = new taskModel({
 		title,
 		description,
@@ -28,19 +37,21 @@ const createTaskService = async (dataObject) => {
 	return newTask;
 };
 
+// Service to get all tasks
 const getAllTasksService = async () => {
 	const tasks = await taskModel.find();
-	if (!tasks) throw new BadRequestError("Not task exist", 400);
+	if (!tasks) throw new BadRequestError("No tasks exist", 400);
 
 	return tasks;
 };
 
+// Service to get a specific task
 const getTaskService = async (dataObject) => {
-	const taskCratedBy = await taskModel.findOne({
+	const taskCreatedBy = await taskModel.findOne({
 		createdBy: dataObject.params.username,
 	});
 
-	if (taskCratedBy) return taskCratedBy;
+	if (taskCreatedBy) return taskCreatedBy;
 
 	const taskAssignedUsername = await taskModel.findOne({
 		assignedTo: dataObject.params.username,
@@ -48,5 +59,12 @@ const getTaskService = async (dataObject) => {
 
 	if (taskAssignedUsername) return taskAssignedUsername;
 
-	throw new BadRequestError("username doesn't exist", 400);
+	throw new BadRequestError("Username doesn't exist", 400);
+};
+
+// Exporting the services so they can be used in other parts of the application
+module.exports = {
+	createTaskService,
+	getAllTasksService,
+	getTaskService,
 };
