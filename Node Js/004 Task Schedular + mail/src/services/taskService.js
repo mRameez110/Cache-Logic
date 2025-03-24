1b31fd62a27b67581a23013659c524e622b768ac
mRameez110 <temprary87@gmail.com>
2025-03-24 17:11:25 +0500

    Added user check in createTaskService

--- Node Js/004 Task Schedular + mail/src/services/taskService.js
+++ Node Js/004 Task Schedular + mail/src/services/taskService.js
@@ -1,3 +1,10 @@
 const createTaskService = async (dataObject) => {
 	const { title, description, createdBy, assignedTo } = dataObject;
+
+	const findTaskCrater = await userModel.findOne({ username: createdBy });
+	if (!findTaskCrater)
+		throw new BadRequestError(
+			"Your are not register. first register to create task",
+			400
+		);
 };
