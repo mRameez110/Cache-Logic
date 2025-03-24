d39d35c7d004b6ac8ae58ed364a790a19d7c3a81
mRameez110 <temprary87@gmail.com>
2025-03-24 17:11:38 +0500

    Added assigned user check in createTaskService

--- Node Js/004 Task Schedular + mail/src/services/taskService.js
+++ Node Js/004 Task Schedular + mail/src/services/taskService.js
@@ -7,4 +7,13 @@ const createTaskService = async (dataObject) => {
 			"Your are not register. first register to create task",
 			400
 		);
+
+	const findTaskAssignedUser = await userModel.findOne({
+		username: assignedTo,
+	});
+	if (!findTaskAssignedUser)
+		throw new BadRequestError(
+			"Assigned user name does't exist. Username must exist to assign task",
+			400
+		);
 };
