31f5e1c3be89fb81a969b2642467ef4f07934469
mRameez110 <temprary87@gmail.com>
2025-03-25 17:23:23 +0500

    read email template from file

--- Node Js/004 Task Schedular + mail/src/services/emailService.js
+++ Node Js/004 Task Schedular + mail/src/services/emailService.js
@@ -5,3 +5,8 @@ const handlebars = require("handlebars");
 
 const { MailError } = require("../utils/constants/errors/errorClass");
 const { taskMailTemplate } = require("../utils/helper/mailTemplate");
+
+const emailTemplateSource = fs.readFileSync(
+  path.join(__dirname, "../utils/helper/mailTemplate.hbs"),
+  "utf8"
+);
