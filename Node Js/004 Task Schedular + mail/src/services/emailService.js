a4900784e83cbc2b80686193f9aef29d52a40e78
mRameez110 <temprary87@gmail.com>
2025-03-25 17:26:25 +0500

    export sendEmail function for use in other modules

--- Node Js/004 Task Schedular + mail/src/services/emailService.js
+++ Node Js/004 Task Schedular + mail/src/services/emailService.js
@@ -42,35 +42,37 @@ const emailTemplateCompile = handlebars.compile(emailTemplateSource);
 console.log("Check email template after compile ", emailTemplateCompile);
 
 const htmlTemplate = emailTemplateCompile({
-  to,
-  subject,
-  title,
-  description,
-  createdBy,
-  assignedTo,
+	to,
+	subject,
+	title,
+	description,
+	createdBy,
+	assignedTo,
 });
 
 console.log("check generated html ", htmlTemplate);
 
 const mailResponse = await transporter.sendMail({
-  from,
-  to,
-  subject,
-  html: htmlTemplate,
-  attachments: [
-    {
-      filename: "logo.jpg",
-      path: path.join(__dirname, "../public", "logo.jpg"),
-      cid: "logo",
-    },
-    {
-      filename: "bidah.PNG",
-      path: path.join(__dirname, "../public/bidah.PNG"),
-    },
-  ],
+	from,
+	to,
+	subject,
+	html: htmlTemplate,
+	attachments: [
+		{
+			filename: "logo.jpg",
+			path: path.join(__dirname, "../public", "logo.jpg"),
+			cid: "logo",
+		},
+		{
+			filename: "bidah.PNG",
+			path: path.join(__dirname, "../public/bidah.PNG"),
+		},
+	],
 });
 
 console.log("Mail Response is ", mailResponse);
 if (!mailResponse) throw new MailError();
 
 return mailResponse;
+
+module.exports = { sendEmail };
