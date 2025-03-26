b10f7157bb5c0e9b0a335b69b460d8b7c446c36e
mRameez110 <temprary87@gmail.com>
2025-03-26 17:16:11 +0500

    Initialize express router and import controller functions for user routes

--- Node Js/004 Task Schedular with dynamic mail template/src/api/routes/userRoute.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/api/routes/userRoute.js
@@ -1,14 +1,9 @@
 const express = require("express");
+
 const {
-  createUser,
-  getAllUsers,
-  getUserByUserName,
+	createUser,
+	getAllUsers,
+	getUserByUserName,
 } = require("../controllers/userController");
 
 const router = express.Router();
-
-router.get("/getUsers", getAllUsers);
-router.post("/createUser", createUser); // done;
-router.get("/user/:username", getUserByUserName);
-
-module.exports = router;
