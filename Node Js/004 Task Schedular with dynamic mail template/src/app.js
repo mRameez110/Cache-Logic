a886bdcd689ece42457d8c2c33df403b457af9f5
mRameez110 <temprary87@gmail.com>
2025-03-26 17:08:09 +0500

    basic app.js

--- Node Js/004 Task Schedular with dynamic mail template/src/app.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/app.js
@@ -1,28 +1,38 @@
-// app.js
-
 require("dotenv").config();
 const express = require("express");
-const userRoute = require("./api/routes/userRoute");
-const taskRoute = require("./api/routes/taskRoute");
-
-app.use("/api/users", userRoute);
-app.use("/api/tasks", taskRoute);
-
-
 const { default: helmet } = require("helmet");
 const cors = require("cors");
 const compression = require("compression");
 
+const userRoute = require("./api/routes/userRoute");
+const taskRoute = require("./api/routes/taskRoute");
+const errorHandler = require("./middlewares/errorHandler");
+const { RouteNotFoundError } = require("./utils/constants/errors/errorClass");
+const connectDB = require("./utils/helper/db/connection");
+
+const app = express();
+
+// Connect to DB
+connectDB();
+
+// Middleware setup
 app.use(helmet());
 app.use(cors());
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-app.get("/", (req, res) => {
-	res.send("Server is running!");
+// Set up routes
+app.use("/api/users", userRoute);
+app.use("/api/tasks", taskRoute);
+
+// Error handling
+app.all("*", (req, res) => {
+    throw new RouteNotFoundError();
 });
 
+app.use(errorHandler);
+
+// Start server
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log("Server started on PORT ", PORT));
