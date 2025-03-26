3ab464e2647f3c918d31f22b1813570f1c0cd0e9
mRameez110 <temprary87@gmail.com>
2025-03-26 17:04:30 +0500

    Add middleware: Helmet, CORS, Compression, JSON Parsing

--- Node Js/004 Task Schedular with dynamic mail template/src/app.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/app.js
@@ -3,6 +3,16 @@
 require("dotenv").config();
 const express = require("express");
 
+const { default: helmet } = require("helmet");
+const cors = require("cors");
+const compression = require("compression");
+
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.get("/", (req, res) => {
