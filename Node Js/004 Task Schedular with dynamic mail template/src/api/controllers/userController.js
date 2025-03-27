0e7012992fd267de43809139aa1eff066ce9dffe
mRameez110 <temprary87@gmail.com>
2025-03-27 17:00:38 +0500

    Add skeleton for getAllUsers function

--- Node Js/004 Task Schedular with dynamic mail template/src/api/controllers/userController.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/api/controllers/userController.js
@@ -1,13 +1,17 @@
 const {
-	registerService,
-	getAllUsersService,
-	getUserService,
+  registerService,
+  getAllUsersService,
+  getUserService,
 } = require("../../services/userAuthService");
 const {
-	registerValidationSchema,
-	validation,
+  registerValidationSchema,
+  validation,
 } = require("../../utils/validations/validations");
 
 const createUser = async (req, res, next) => {
-	// Function implementation will be added later
+  // Function implementation will be added later
+};
+
+const getAllUsers = async (req, res, next) => {
+  // Function implementation will be added later
 };
