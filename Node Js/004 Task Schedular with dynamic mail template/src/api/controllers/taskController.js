35a9d325f61f72e47225d0a01dbc76f8a5e44834
mRameez110 <temprary87@gmail.com>
2025-03-28 16:52:50 +0500

    created taskController.js file with initial imports

---
+++ Node Js/004 Task Schedular with dynamic mail template/src/api/controllers/taskController.js
@@ -0,0 +1,13 @@
+
+const {
+	createTaskService,
+	getAllTasksService,
+	getTaskService,
+} = require("../../services/taskService");
+const userModel = require("../../models/userModel");
+const { sendEmail } = require("../../services/emailService");
+const {
+	taskCreateValidationSchema,
+	validation,
+} = require("../../utils/validations/validations");
+const { response } = require("express");
