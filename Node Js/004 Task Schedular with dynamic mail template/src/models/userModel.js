ba3d891cfda8fe2806ca379ae2046afb7f4384f0
mRameez110 <temprary87@gmail.com>
2025-04-03 20:28:08 +0500

    Added email field to user schema.

--- Node Js/004 Task Schedular with dynamic mail template/src/models/userModel.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/models/userModel.js
@@ -1,13 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-  },
-  { timestamps: true }
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+	},
+	{ timestamps: true }
 );
 
