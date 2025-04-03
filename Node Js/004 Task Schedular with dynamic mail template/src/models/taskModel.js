4dd30f472e0dd1884af3a05b96eedc8cb3fc8fb5
mRameez110 <temprary87@gmail.com>
2025-04-03 20:33:51 +0500

    Refactored task schema for readability

--- Node Js/004 Task Schedular with dynamic mail template/src/models/taskModel.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/models/taskModel.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+// Define the task schema with fields title, description, createdBy, and assignedTo
 const taskSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    description: String,
-    createdBy: {
-      type: String,
-      required: true,
-    },
-    assignedTo: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		description: String,
+		createdBy: {
+			type: String,
+			required: true,
+		},
+		assignedTo: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
 );
