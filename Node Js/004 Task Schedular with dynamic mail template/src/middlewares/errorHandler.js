fa15c5782718a8d411a78e16d743773abf318706
mRameez110 <temprary87@gmail.com>
2025-04-04 20:31:55 +0500

    Add default error message and errorCode

--- Node Js/004 Task Schedular with dynamic mail template/src/middlewares/errorHandler.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/middlewares/errorHandler.js
@@ -1,6 +1,5 @@
-// errorHandler.js
 const errorHandler = (err, req, res, next) => {
-
+	console.error(err.stack);
+	const errorMessage = err.message || "Something Bad";
+	const errorCode = err.errorCode || 500;
 };
-
-module.exports = errorHandler;
