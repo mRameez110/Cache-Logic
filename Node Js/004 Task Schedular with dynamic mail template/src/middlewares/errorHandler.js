114682aa03434ee3f182be7dc62446de4ef16a50
mRameez110 <temprary87@gmail.com>
2025-04-04 20:33:18 +0500

    completed error handling logic

--- Node Js/004 Task Schedular with dynamic mail template/src/middlewares/errorHandler.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/middlewares/errorHandler.js
@@ -1,7 +1,9 @@
 const errorHandler = (err, req, res, next) => {
 	console.error(err.stack);
+
 	const errorMessage = err.message || "Something Bad";
 	const errorCode = err.errorCode || 500;
+
 	res.status(errorCode).json({
 		status: false,
 		message: errorMessage,
