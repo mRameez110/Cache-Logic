1da40079580bd447d4867df32b84371e0a27b37e
mRameez110 <temprary87@gmail.com>
2025-04-08 20:25:36 +0500

    Add heading for task notification

--- Node Js/004 Task Schedular with dynamic mail template/src/helper/mailTemplate.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/helper/mailTemplate.js
@@ -4,4 +4,7 @@ const taskMailTemplate = (
 	createdBy,
 	assignedTo
 ) => `<div style="font-family: Arial, sans-serif; line-height: 1.6; color: black;">
+
+<h2 style="color: green;">Task Notification</h2>
+
 </div>`;
