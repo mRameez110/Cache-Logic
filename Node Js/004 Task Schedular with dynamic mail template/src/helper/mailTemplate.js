711e8918882aa3cc87b6763d738228018e5b669d
mRameez110 <temprary87@gmail.com>
2025-04-08 20:26:04 +0500

    Add dynamic field for task title

--- Node Js/004 Task Schedular with dynamic mail template/src/helper/mailTemplate.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/helper/mailTemplate.js
@@ -6,5 +6,7 @@ const taskMailTemplate = (
 ) => `<div style="font-family: Arial, sans-serif; line-height: 1.6; color: black;">
 
 <h2 style="color: green;">Task Notification</h2>
+<h4><strong>Task Title:</strong> ${title}</h4>
+
 
 </div>`;
