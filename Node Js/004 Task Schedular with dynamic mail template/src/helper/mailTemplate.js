1a74a70c800fb16b9bbb221078b376dc9d6d62a3
mRameez110 <temprary87@gmail.com>
2025-04-08 20:27:19 +0500

    Add dynamic field for assigned user

--- Node Js/004 Task Schedular with dynamic mail template/src/helper/mailTemplate.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/helper/mailTemplate.js
@@ -9,6 +9,7 @@ const taskMailTemplate = (
 <h4><strong>Task Title:</strong> ${title}</h4>
 <p><strong>Task Description:</strong> ${description}</p>
 <p><strong>Task Created By:</strong> ${createdBy}</p>
+<p><strong>Task Assigned To:</strong> ${assignedTo}</p>
 
 
 </div>`;
