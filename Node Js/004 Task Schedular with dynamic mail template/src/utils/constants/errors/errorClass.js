feb0c53342af59fb567f4bc7c887069681a1fd91
mRameez110 <temprary87@gmail.com>
2025-04-09 21:15:47 +0500

    Add ForbiddenError for forbidden actions

--- Node Js/004 Task Schedular with dynamic mail template/src/utils/constants/errors/errorClass.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/utils/constants/errors/errorClass.js
@@ -1,37 +1,43 @@
 class AppError extends Error {
-	constructor(message = "Something is wrong", errorCode = 500) {
-		super();
-		this.message = message;
-		this.errorCode = errorCode;
-	}
+  constructor(message = "Something is wrong", errorCode = 500) {
+    super();
+    this.message = message;
+    this.errorCode = errorCode;
+  }
 }
 
 class RouteNotFoundError extends AppError {
-	constructor(message = "Route does't exist", errorCode = 404) {
-		super(message, errorCode);
-	}
+  constructor(message = "Route does't exist", errorCode = 404) {
+    super(message, errorCode);
+  }
 }
 
 class UserAlreadyExistError extends AppError {
-	constructor(message = "User Already Exist", errorCode = 400) {
-		super(message, errorCode);
-	}
+  constructor(message = "User Already Exist", errorCode = 400) {
+    super(message, errorCode);
+  }
 }
 
 class BadRequestError extends AppError {
-	constructor(message = "Bad Request", errorCode = 400) {
-		super(message, errorCode);
-	}
+  constructor(message = "Bad Request", errorCode = 400) {
+    super(message, errorCode);
+  }
 }
 
 class MailError extends AppError {
-	constructor(message = "Error in Mail generation/transfer", errorCode = 500) {
-		super(message, errorCode);
-	}
+  constructor(message = "Error in Mail generation/transfer", errorCode = 500) {
+    super(message, errorCode);
+  }
 }
 
 class UnauthorizedError extends AppError {
-	constructor(message = "Unauthorized access", errorCode = 401) {
-		super(message, errorCode);
-	}
+  constructor(message = "Unauthorized access", errorCode = 401) {
+    super(message, errorCode);
+  }
+}
+
+class ForbiddenError extends AppError {
+  constructor(message = "Forbidden action", errorCode = 403) {
+    super(message, errorCode);
+  }
 }
