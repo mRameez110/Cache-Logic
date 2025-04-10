ea93c0996ddecf7fe6dc6e39060e74e47c2f4de4
mRameez110 <temprary87@gmail.com>
2025-04-10 20:38:30 +0500

    implement getAllUsersService to fetch all users

--- Node Js/004 Task Schedular with dynamic mail template/src/services/userAuthService.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/services/userAuthService.js
@@ -1,33 +1,39 @@
 const bcrypt = require("bcryptjs");
 const userModel = require("..//models/userModel");
 const {
-  UserAlreadyExistError,
-  BadRequestError,
+	UserAlreadyExistError,
+	BadRequestError,
 } = require("../utils/constants/errors/errorClass");
 
 const registerService = async (dataObject) => {
-  const { username, email, password } = dataObject;
-  const userNameAlreadyExist = await userModel.findOne({ username });
+	const { username, email, password } = dataObject;
+	const userNameAlreadyExist = await userModel.findOne({ username });
 
-  if (userNameAlreadyExist) {
-    throw new UserAlreadyExistError("Username already in use", 400);
-  }
+	if (userNameAlreadyExist) {
+		throw new UserAlreadyExistError("Username already in use", 400);
+	}
 
-  const userEmailAlreadyExist = await userModel.findOne({ email });
+	const userEmailAlreadyExist = await userModel.findOne({ email });
 
-  if (userEmailAlreadyExist) {
-    throw new UserAlreadyExistError("Email already in use", 400);
-  }
-  const saltRounds = 10;
-  const hashPassword = await bcrypt.hash(password, saltRounds);
+	if (userEmailAlreadyExist) {
+		throw new UserAlreadyExistError("Email already in use", 400);
+	}
+	const saltRounds = 10;
+	const hashPassword = await bcrypt.hash(password, saltRounds);
 
-  const registerNewUser = new userModel({
-    username,
-    email,
-    password: hashPassword,
-  });
+	const registerNewUser = new userModel({
+		username,
+		email,
+		password: hashPassword,
+	});
 
-  const user = await registerNewUser.save();
-  return user;
+	const user = await registerNewUser.save();
+	return user;
 };
 
+const getAllUsersService = async () => {
+	const users = await userModel.find();
+	if (!users) throw new BadRequestError("Not get the users");
+
+	return users;
+};
