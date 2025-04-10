c2894d69ae3d971e2b865e5a010ff894989636b0
mRameez110 <temprary87@gmail.com>
2025-04-10 20:30:30 +0500

    Add task creation validation schema

--- Node Js/004 Task Schedular with dynamic mail template/src/utils/validations/validations.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/utils/validations/validations.js
@@ -7,3 +7,12 @@ const registerValidationSchema = joi
 		password: joi.string().min(1).required(),
 	})
 	.options({ abortEarly: true });
+
+const taskCreateValidationSchema = joi
+	.object({
+		title: joi.string().min(5).max(15).trim().required(),
+		description: joi.string().max(50).trim(),
+		createdBy: joi.string().min(2).max(10).trim().required(),
+		assignedTo: joi.string().min(2).max(10).trim().required(),
+	})
+	.options({ abortEarly: true });
