b407ab9f09029327c9098edd4ed6657136a8b604
mRameez110 <temprary87@gmail.com>
2025-04-11 20:31:18 +0500

    saved new task and returned it from service

--- Node Js/004 Task Schedular with dynamic mail template/src/services/taskService.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/services/taskService.js
@@ -3,27 +3,28 @@ const userModel = require("../models/userModel");
 const { BadRequestError } = require("../utils/constants/errors/errorClass");
 
 const createTaskService = async (dataObject) => {
-  const { title, description, createdBy, assignedTo } = dataObject;
-  const findTaskCrater = await userModel.findOne({ username: createdBy });
-  console.log("Task crater finding ", findTaskCrater);
-  if (!findTaskCrater)
-    throw new BadRequestError(
-      "Your are not register. first register to create task",
-      400
-    );
-    const findTaskAssignedUser = await userModel.findOne({
-      username: assignedTo,
-    });
-    if (!findTaskAssignedUser)
-      throw new BadRequestError(
-        "Assigned user name does't exist. Username must exist to assign task",
-        400
-      );
-      const newTask = new taskModel({
-        title,
-        description,
-        createdBy,
-        assignedTo,
-      });
-    
-  
+	const { title, description, createdBy, assignedTo } = dataObject;
+	const findTaskCrater = await userModel.findOne({ username: createdBy });
+	console.log("Task crater finding ", findTaskCrater);
+	if (!findTaskCrater)
+		throw new BadRequestError(
+			"Your are not register. first register to create task",
+			400
+		);
+	const findTaskAssignedUser = await userModel.findOne({
+		username: assignedTo,
+	});
+	if (!findTaskAssignedUser)
+		throw new BadRequestError(
+			"Assigned user name does't exist. Username must exist to assign task",
+			400
+		);
+	const newTask = new taskModel({
+		title,
+		description,
+		createdBy,
+		assignedTo,
+	});
+	await newTask.save();
+	return newTask;
+};
