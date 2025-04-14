f14ec02c72be68fe5dd2fb6ebad43eab1e3c18b9
mRameez110 <temprary87@gmail.com>
2025-04-14 20:44:40 +0500

    imported local modules

--- Node Js/004 Task Schedular with dynamic mail template/src/services/emailService.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/services/emailService.js
@@ -2,3 +2,5 @@ const nodemailer = require("nodemailer");
 const fs = require("fs");
 const path = require("path");
 const handlebars = require("handlebars");
+const { MailError } = require("../utils/constants/errors/errorClass");
+const { taskMailTemplate } = require("../utils/helper/mailTemplate");
