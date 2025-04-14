24d46361d1c034d8ac242ef4dd421fd5d77f4b9d
mRameez110 <temprary87@gmail.com>
2025-04-14 20:44:53 +0500

    read and loaded email template from mailTemplate.hbs

--- Node Js/004 Task Schedular with dynamic mail template/src/services/emailService.js
+++ Node Js/004 Task Schedular with dynamic mail template/src/services/emailService.js
@@ -4,3 +4,7 @@ const path = require("path");
 const handlebars = require("handlebars");
 const { MailError } = require("../utils/constants/errors/errorClass");
 const { taskMailTemplate } = require("../utils/helper/mailTemplate");
+const emailTemplateSource = fs.readFileSync(
+	path.join(__dirname, "../utils/helper/mailTemplate.hbs"),
+	"utf8"
+);
