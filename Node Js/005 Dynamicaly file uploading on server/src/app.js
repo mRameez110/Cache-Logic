e84d7192f02fe78f3bed1a6651ec15a266397e6c
mRameez110 <temprary87@gmail.com>
2025-04-15 20:33:55 +0500

    imported packages

---
+++ Node Js/005 Dynamicaly file uploading on server/src/app.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const express = require("express");
+const { default: helmet } = require("helmet");
+const cors = require("cors");
+const compression = require("compression");
+const bodyParser = require("body-parser");
