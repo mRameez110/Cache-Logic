fc3c73053b10f1547784edfce3d856fb5c64e6df
mRameez110 <temprary87@gmail.com>
2025-04-17 21:06:24 +0500

    enable timestamps for createdAt and updatedAt fields

--- Node Js/005 Dynamicaly file uploading on server/src/models/taskModel.js
+++ Node Js/005 Dynamicaly file uploading on server/src/models/taskModel.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 const taskSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      require: true,
-    },
-    description: String,
-    createdBy: {
-      type: String,
-      required: true,
-    },
-    assignedTo: {
-      type: String,
-      required: true,
-    },
+	{
+		title: {
+			type: String,
+			require: true,
+		},
+		description: String,
+		createdBy: {
+			type: String,
+			required: true,
+		},
+		assignedTo: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
