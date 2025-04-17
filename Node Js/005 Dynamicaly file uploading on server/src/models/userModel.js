5a6142c9037d8177d6b9b9a124d86e3b6a039795
mRameez110 <temprary87@gmail.com>
2025-04-17 21:03:21 +0500

    define userSchema with username field

--- Node Js/005 Dynamicaly file uploading on server/src/models/userModel.js
+++ Node Js/005 Dynamicaly file uploading on server/src/models/userModel.js
@@ -1,2 +1,9 @@
 const mongoose = require("mongoose");
-const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
