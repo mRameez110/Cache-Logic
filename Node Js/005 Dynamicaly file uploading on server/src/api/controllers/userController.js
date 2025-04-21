cc4e117f9e7cab8c0063f62da5e1af79cbbb4bb6
mRameez110 <temprary87@gmail.com>
2025-04-21 20:30:29 +0500

    fix bracket missign bug

--- Node Js/005 Dynamicaly file uploading on server/src/api/controllers/userController.js
+++ Node Js/005 Dynamicaly file uploading on server/src/api/controllers/userController.js
@@ -3,46 +3,52 @@ const {
 	getAllUsersService,
 	getUserService,
 } = require("../../services/userAuthService");
-
 const {
 	registerValidationSchema,
 	validation,
 } = require("../../utils/validations/validations");
 
 const createUser = async (req, res, next) => {
-  try {
-    validation(req.body, registerValidationSchema);
-    const result = await registerService(req.body);
-    res.status(200).json({
-      status: true,
-      message: "User created Successfully",
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-  
-  const getAllUsers = async (req, res, next) => {
-    try {
-      const users = await getAllUsersService();
-    
-      res.status(200).json({
-        status: true,
-        message: "All user request successful",
-        data: users,
-      });
-    } catch (error) {
-      next(error);
-    }
-    const getUserByUserName = async (req, res, next) => {
-      try {
-        const user = await getUserService(req);
-        res.status(200).json({
-          status: true,
-          message: "user details are below",
-          data: user,
-        });
-      } catch (error) {
-        next(error);
-      }
-      module.exports = { createUser, getAllUsers, getUserByUserName };
+	try {
+		validation(req.body, registerValidationSchema);
+		const result = await registerService(req.body);
+
+		res.status(200).json({
+			status: true,
+			message: "User created Successfully",
+			data: result,
+		});
+	} catch (err) {
+		next(err);
+	}
+};
+
+const getAllUsers = async (req, res, next) => {
+	try {
+		const users = await getAllUsersService();
+
+		res.status(200).json({
+			status: true,
+			message: "All user request successful",
+			data: users,
+		});
+	} catch (error) {
+		next(error);
+	}
+};
+
+const getUserByUserName = async (req, res, next) => {
+	try {
+		const user = await getUserService(req);
+
+		res.status(200).json({
+			status: true,
+			message: "user details are below",
+			data: user,
+		});
+	} catch (error) {
+		next(error);
+	}
+};
+
+module.exports = { createUser, getAllUsers, getUserByUserName };
