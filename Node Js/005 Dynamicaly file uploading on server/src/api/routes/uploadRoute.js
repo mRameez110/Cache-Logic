b04f14467c9cff78b38158dda404ab9f10ff0b06
mRameez110 <temprary87@gmail.com>
2025-04-21 20:23:42 +0500

    fixed bracket bug

--- Node Js/005 Dynamicaly file uploading on server/src/api/routes/uploadRoute.js
+++ Node Js/005 Dynamicaly file uploading on server/src/api/routes/uploadRoute.js
@@ -1,19 +1,20 @@
 const express = require("express");
-const upload = require("../../utils/helper/upload");
 
+const upload = require("../../utils/helper/upload");
 const router = express.Router();
 
 router.post("/", upload.single("image"), (req, res) => {
+	if (!req.file) {
+		return res.status(400).send("No file uploaded or invalid file type.");
+	}
 
-  if (!req.file) {
-    return res.status(400).send("No file uploaded or invalid file type.");
-  }
-  res.send({
-    message: "File uploaded successfully",
-    filename: req.file.filename,
-    file: req.file,
-    type: req.body.type,
-    path: req.body.path,
-  });
-  
-  module.exports = router;
+	res.send({
+		message: "File uploaded successfully",
+		filename: req.file.filename,
+		file: req.file,
+		type: req.body.type,
+		path: req.body.path,
+	});
+});
+
+module.exports = router;
