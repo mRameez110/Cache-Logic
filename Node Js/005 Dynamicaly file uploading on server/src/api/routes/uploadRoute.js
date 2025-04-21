14d6812bd28b22b86389a297d9ccae750947aaf0
mRameez110 <temprary87@gmail.com>
2025-04-21 20:18:45 +0500

    define POST route to handle single file upload

--- Node Js/005 Dynamicaly file uploading on server/src/api/routes/uploadRoute.js
+++ Node Js/005 Dynamicaly file uploading on server/src/api/routes/uploadRoute.js
@@ -2,3 +2,5 @@ const express = require("express");
 const upload = require("../../utils/helper/upload");
 
 const router = express.Router();
+
+router.post("/", upload.single("image"), (req, res) => {
