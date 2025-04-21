a31fa9a47a166e9508fd855839a4259bfd22bdcd
mRameez110 <temprary87@gmail.com>
2025-04-21 20:19:10 +0500

    send response with uploaded file details

--- Node Js/005 Dynamicaly file uploading on server/src/api/routes/uploadRoute.js
+++ Node Js/005 Dynamicaly file uploading on server/src/api/routes/uploadRoute.js
@@ -8,4 +8,11 @@ router.post("/", upload.single("image"), (req, res) => {
   if (!req.file) {
     return res.status(400).send("No file uploaded or invalid file type.");
   }
+  res.send({
+    message: "File uploaded successfully",
+    filename: req.file.filename,
+    file: req.file,
+    type: req.body.type,
+    path: req.body.path,
+  });
   
