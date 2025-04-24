05bb96ab2724f3a5ea07e591eadf55db7d9ec6d5
mRameez110 <temprary87@gmail.com>
2025-04-24 20:56:15 +0500

    set errorMessage from error or fallback message

--- Node Js/005 Dynamicaly file uploading on server/src/middlewares/errorHandler.js
+++ Node Js/005 Dynamicaly file uploading on server/src/middlewares/errorHandler.js
@@ -1,3 +1,4 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
   // console.log("What are properties of error ", err);
+  const errorMessage = err.message || "Something Bad";
