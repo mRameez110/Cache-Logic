a4c0bb5d7c4461926a1383ffdfb3697fbf1a5cac
mRameez110 <temprary87@gmail.com>
2025-04-24 20:58:02 +0500

    close errorHandler function

--- Node Js/005 Dynamicaly file uploading on server/src/middlewares/errorHandler.js
+++ Node Js/005 Dynamicaly file uploading on server/src/middlewares/errorHandler.js
@@ -1,10 +1,11 @@
 const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  // console.log("What are properties of error ", err);
-  const errorMessage = err.message || "Something Bad";
-  const errorCode = err.errorCode || 500;
-  res.status(errorCode).json({
-    status: false,
-    message: errorMessage,
-    data: null,
-  });
+	console.error(err.stack);
+	// console.log("What are properties of error ", err);
+	const errorMessage = err.message || "Something Bad";
+	const errorCode = err.errorCode || 500;
+	res.status(errorCode).json({
+		status: false,
+		message: errorMessage,
+		data: null,
+	});
+};
