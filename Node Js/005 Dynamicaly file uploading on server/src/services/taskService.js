66b33d02655f086631d223d784100bb6c20de5cb
mRameez110 <temprary87@gmail.com>
2025-04-28 21:09:45 +0500

    destructure title, description, createdBy, and assignedTo from input

--- Node Js/005 Dynamicaly file uploading on server/src/services/taskService.js
+++ Node Js/005 Dynamicaly file uploading on server/src/services/taskService.js
@@ -3,3 +3,4 @@ const userModel = require("../models/userModel");
 const { BadRequestError } = require("../utils/constants/errors/errorClass");
 
 const createTaskService = async (dataObject) => {
+  const { title, description, createdBy, assignedTo } = dataObject;
