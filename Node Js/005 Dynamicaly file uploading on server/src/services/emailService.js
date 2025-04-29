12719daffa7de0af0b2c9f654fb462e39df2ded1
mRameez110 <temprary87@gmail.com>
2025-04-29 20:18:51 +0500

    read handlebars template file from disk

--- Node Js/005 Dynamicaly file uploading on server/src/services/emailService.js
+++ Node Js/005 Dynamicaly file uploading on server/src/services/emailService.js
@@ -2,6 +2,10 @@ const nodemailer = require("nodemailer");
 const fs = require("fs");
 const path = require("path");
 const handlebars = require("handlebars");
-
 const { MailError } = require("../utils/constants/errors/errorClass");
 const { taskMailTemplate } = require("../utils/helper/mailTemplate");
+
+const emailTemplateSource = fs.readFileSync(
+	path.join(__dirname, "../utils/helper/mailTemplate.hbs"),
+	"utf8"
+);
