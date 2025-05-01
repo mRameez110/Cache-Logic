9e17d07d448f8dd31fe7e776201a0337f0ec9f98
mRameez110 <temprary87@gmail.com>
2025-05-01 20:31:14 +0500

    extract file type from request body

--- Node Js/005 Dynamicaly file uploading on server/src/utils/helper/upload.js
+++ Node Js/005 Dynamicaly file uploading on server/src/utils/helper/upload.js
@@ -3,4 +3,6 @@ const path = require("path");
 const fs = require("fs");
 const baseDir = path.join(__dirname, "../../public/uploadDir");
 const storage = multer.diskStorage({});
-destination: (req, file, cb) => { ... }
+destination: (req, file, cb) => {
+	const type = req.body.type;
+};
