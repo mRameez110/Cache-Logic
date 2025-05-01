e9f24e4e895e13497724d253745d2bedc6326da2
mRameez110 <temprary87@gmail.com>
2025-05-01 20:30:32 +0500

    add destination function for storage

--- Node Js/005 Dynamicaly file uploading on server/src/utils/helper/upload.js
+++ Node Js/005 Dynamicaly file uploading on server/src/utils/helper/upload.js
@@ -3,3 +3,4 @@ const path = require("path");
 const fs = require("fs");
 const baseDir = path.join(__dirname, "../../public/uploadDir");
 const storage = multer.diskStorage({});
+destination: (req, file, cb) => { ... }
