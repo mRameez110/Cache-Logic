c7905c5e5a670d54e25574bdf3379d6069614efc
mRameez110 <temprary87@gmail.com>
2025-05-06 20:52:15 +0500

    handle DB connection errors with logging

--- Node Js/005 Dynamicaly file uploading on server/src/utils/helper/db/connection.js
+++ Node Js/005 Dynamicaly file uploading on server/src/utils/helper/db/connection.js
@@ -6,6 +6,9 @@ const connectDB = () => {
 	if (dbURL) {
 		mongoose
 			.connect(dbURL)
-			.then(() => console.log("Database connected successfully"));
+			.then(() => console.log("Database connected successfully"))
+			.catch((err) =>
+				console.log("Something wrong in connecting Mongo DB ", err)
+			);
 	}
 };
