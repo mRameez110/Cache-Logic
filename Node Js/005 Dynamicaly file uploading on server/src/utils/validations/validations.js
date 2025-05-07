145d5b69c3611e32d3b00813451f89b9ca673a43
mRameez110 <temprary87@gmail.com>
2025-05-07 20:55:15 +0500

    finalize register schema with abortEarly option

--- Node Js/005 Dynamicaly file uploading on server/src/utils/validations/validations.js
+++ Node Js/005 Dynamicaly file uploading on server/src/utils/validations/validations.js
@@ -1,6 +1,8 @@
 const joi = require("joi");
-const registerValidationSchema = joi.object({
-	username: joi.string().min(3).max(50).trim().required(),
-	email: joi.string().email().min(1).max(50).trim().required(),
-	password: joi.string().min(1).required(),
-});
+const registerValidationSchema = joi
+	.object({
+		username: joi.string().min(3).max(50).trim().required(),
+		email: joi.string().email().min(1).max(50).trim().required(),
+		password: joi.string().min(1).required(),
+	})
+	.options({ abortEarly: true });
