3ae43313f979857235b5fbfc9572ec252068ff61
mRameez110 <temprary87@gmail.com>
2025-05-07 20:54:28 +0500

    add username field to register schema

--- Node Js/005 Dynamicaly file uploading on server/src/utils/validations/validations.js
+++ Node Js/005 Dynamicaly file uploading on server/src/utils/validations/validations.js
@@ -1,2 +1,4 @@
 const joi = require("joi");
-const registerValidationSchema = joi.object({});
+const registerValidationSchema = joi.object({
+	username: joi.string().min(3).max(50).trim().required(),
+});
