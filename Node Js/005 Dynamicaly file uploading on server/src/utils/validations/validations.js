73f8b8f6ca41de7601368355db537a24b5c4f9f7
mRameez110 <temprary87@gmail.com>
2025-05-07 20:54:48 +0500

    add password field to register schema

--- Node Js/005 Dynamicaly file uploading on server/src/utils/validations/validations.js
+++ Node Js/005 Dynamicaly file uploading on server/src/utils/validations/validations.js
@@ -2,4 +2,5 @@ const joi = require("joi");
 const registerValidationSchema = joi.object({
 	username: joi.string().min(3).max(50).trim().required(),
 	email: joi.string().email().min(1).max(50).trim().required(),
+	password: joi.string().min(1).required(),
 });
