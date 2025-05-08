9402ea88018960eb3163b942d6bde16f92c4babc
mRameez110 <temprary87@gmail.com>
2025-05-08 20:58:15 +0500

    set abortEarly true for task creation validation schema

--- Node Js/005 Dynamicaly file uploading on server/src/utils/validations/validations.js
+++ Node Js/005 Dynamicaly file uploading on server/src/utils/validations/validations.js
@@ -7,9 +7,11 @@ const registerValidationSchema = joi
 	})
 	.options({ abortEarly: true });
 
-const taskCreateValidationSchema = joi.object({
-	title: joi.string().min(5).max(15).trim().required(),
-	description: joi.string().max(50).trim(),
-	createdBy: joi.string().min(2).max(10).trim().required(),
-	assignedTo: joi.string().min(2).max(10).trim().required(),
-});
+const taskCreateValidationSchema = joi
+	.object({
+		title: joi.string().min(5).max(15).trim().required(),
+		description: joi.string().max(50).trim(),
+		createdBy: joi.string().min(2).max(10).trim().required(),
+		assignedTo: joi.string().min(2).max(10).trim().required(),
+	})
+	.options({ abortEarly: true });
