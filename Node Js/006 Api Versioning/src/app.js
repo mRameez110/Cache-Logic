a03b0fccea8d9194142560ef17b579c0d693e209
mRameez110 <temprary87@gmail.com>
2025-05-12 20:11:16 +0500

    import version 1 user and task routes

--- Node Js/006 Api Versioning/src/app.js
+++ Node Js/006 Api Versioning/src/app.js
@@ -2,3 +2,6 @@ require("dotenv").config();
 const { default: helmet } = require("helmet");
 const cors = require("cors");
 const compression = require("compression");
+
+const userRouteV1 = require("./api/v1/routes/userRoute");
+const taskRouteV1 = require("./api/v1/routes/taskRoute");
