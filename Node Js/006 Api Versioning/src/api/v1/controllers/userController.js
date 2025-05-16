caf62ce971c47d31396b69e1f4c6cad695ba8244
mRameez110 <temprary87@gmail.com>
2025-05-16 20:05:46 +0500

    import validation utilities and registerValidationSchema

--- Node Js/006 Api Versioning/src/api/v1/controllers/userController.js
+++ Node Js/006 Api Versioning/src/api/v1/controllers/userController.js
@@ -3,3 +3,7 @@ const {
 	getAllUsersService,
 	getUserService,
 } = require("../../../services/userAuthService");
+const {
+	registerValidationSchema,
+	validation,
+} = require("../../../utils/validations/validations");
