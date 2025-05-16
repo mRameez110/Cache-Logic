4f0905352ac3220753a241bd3802c32270aa02ff
mRameez110 <temprary87@gmail.com>
2025-05-16 20:22:46 +0500

    add registerService call and response in createUser

--- Node Js/006 Api Versioning/src/api/v1/controllers/userController.js
+++ Node Js/006 Api Versioning/src/api/v1/controllers/userController.js
@@ -10,11 +10,13 @@ const {
 
 const createUser = async (req, res, next) => {
 	try {
-    validation(req.body, registerValidationSchema);
-bash
-Copy
-Edit
-
+		validation(req.body, registerValidationSchema);
+		const result = await registerService(req.body);
+		res.status(200).json({
+			status: true,
+			message: "User created Successfully",
+			data: result,
+		});
 	} catch (err) {
 		next(err);
 	}
