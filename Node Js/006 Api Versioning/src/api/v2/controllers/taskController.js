23d26cfbe922cf6d9e03002c63e24c879af2c5c5
mRameez110 <temprary87@gmail.com>
2025-05-27 20:41:42 +0500

    import getTaskService from taskService

--- Node Js/006 Api Versioning/src/api/v2/controllers/taskController.js
+++ Node Js/006 Api Versioning/src/api/v2/controllers/taskController.js
@@ -1,2 +1,3 @@
 const { createTaskService } = require("../../../services/taskService");
 const { getAllTasksService } = require("../../../services/taskService");
+const { getTaskService } = require("../../../services/taskService");
