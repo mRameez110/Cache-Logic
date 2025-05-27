719a623bb7c49e9a5464b0bb9697cee7d35b530e
mRameez110 <temprary87@gmail.com>
2025-05-27 20:42:03 +0500

    import sendEmail from emailService

--- Node Js/006 Api Versioning/src/api/v2/controllers/taskController.js
+++ Node Js/006 Api Versioning/src/api/v2/controllers/taskController.js
@@ -2,3 +2,4 @@ const { createTaskService } = require("../../../services/taskService");
 const { getAllTasksService } = require("../../../services/taskService");
 const { getTaskService } = require("../../../services/taskService");
 const userModel = require("../../../models/userModel");
+const { sendEmail } = require("../../../services/emailService");
