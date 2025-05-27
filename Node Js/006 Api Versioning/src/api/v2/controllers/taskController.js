d1ebc25e8e6fb09f02877a763ba489d7447edd2e
mRameez110 <temprary87@gmail.com>
2025-05-27 20:41:51 +0500

    import userModel

--- Node Js/006 Api Versioning/src/api/v2/controllers/taskController.js
+++ Node Js/006 Api Versioning/src/api/v2/controllers/taskController.js
@@ -1,3 +1,4 @@
 const { createTaskService } = require("../../../services/taskService");
 const { getAllTasksService } = require("../../../services/taskService");
 const { getTaskService } = require("../../../services/taskService");
+const userModel = require("../../../models/userModel");
