9df470878e6c2868587eea970d52358d8fb64fd4
mRameez110 <temprary87@gmail.com>
2025-05-27 20:42:16 +0500

    import taskCreateValidationSchema

--- Node Js/006 Api Versioning/src/api/v2/controllers/taskController.js
+++ Node Js/006 Api Versioning/src/api/v2/controllers/taskController.js
@@ -3,3 +3,6 @@ const { getAllTasksService } = require("../../../services/taskService");
 const { getTaskService } = require("../../../services/taskService");
 const userModel = require("../../../models/userModel");
 const { sendEmail } = require("../../../services/emailService");
+const {
+	taskCreateValidationSchema,
+} = require("../../../utils/validations/validations");
