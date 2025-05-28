d5f66164b8e51bac48d80ebc389147cc77feb912
mRameez110 <temprary87@gmail.com>
2025-05-28 19:59:52 +0500

    call createTaskService in createTask

--- Node Js/006 Api Versioning/src/api/v2/controllers/taskController.js
+++ Node Js/006 Api Versioning/src/api/v2/controllers/taskController.js
@@ -11,6 +11,8 @@ const { validation } = require("../../../utils/validations/validations");
 const createTask = async (req, res, next) => {
 	try {
 		validation(req.body, taskCreateValidationSchema);
+		const newTask = await createTaskService(req.body);
+
 	} catch (err) {
 		next(err);
 	}
