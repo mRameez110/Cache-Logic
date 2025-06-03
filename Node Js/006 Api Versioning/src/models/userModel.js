cbb4af098195d1e8119c17a3ffbeb3772b7c48c5
mRameez110 <temprary87@gmail.com>
2025-06-03 20:07:29 +0500

    add username field to userSchema

--- Node Js/006 Api Versioning/src/models/userModel.js
+++ Node Js/006 Api Versioning/src/models/userModel.js
@@ -1,2 +1,8 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+});
