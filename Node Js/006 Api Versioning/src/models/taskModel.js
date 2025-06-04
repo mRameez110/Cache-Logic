582fe8f288cc62d8a8f44468aeddcbb47cfc6c8d
mRameez110 <temprary87@gmail.com>
2025-06-04 19:57:44 +0500

    add title field with type and required to task schema

--- Node Js/006 Api Versioning/src/models/taskModel.js
+++ Node Js/006 Api Versioning/src/models/taskModel.js
@@ -1,3 +1,8 @@
 const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema({});
+const taskSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		require: true,
+	},
+});
