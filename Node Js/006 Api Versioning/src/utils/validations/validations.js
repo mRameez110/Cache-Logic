c9ea10db00a18e54e5bf907d631a7421bf4b72c2
mRameez110 <temprary87@gmail.com>
2025-06-10 20:21:24 +0500

    add createdBy validation to task schema

--- Node Js/006 Api Versioning/src/utils/validations/validations.js
+++ Node Js/006 Api Versioning/src/utils/validations/validations.js
@@ -12,5 +12,6 @@ const taskCreateValidationSchema = joi
 	.object({
 		title: joi.string().min(5).max(15).trim().required(),
 		description: joi.string().max(50).trim(),
+		createdBy: joi.string().min(2).max(10).trim().required(),
 	})
 	.options({ abortEarly: true });
