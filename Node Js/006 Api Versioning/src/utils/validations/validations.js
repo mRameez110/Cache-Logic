d73b6d8371566322ce3b4f4ad471b76dea061de6
mRameez110 <temprary87@gmail.com>
2025-06-10 20:17:50 +0500

    add email validation to register schema

--- Node Js/006 Api Versioning/src/utils/validations/validations.js
+++ Node Js/006 Api Versioning/src/utils/validations/validations.js
@@ -3,5 +3,6 @@ const joi = require("joi");
 const registerValidationSchema = joi
 	.object({
 		username: joi.string().min(3).max(50).trim().required(),
+		email: joi.string().email().min(1).max(50).trim().required(),
 	})
 	.options({ abortEarly: true });
