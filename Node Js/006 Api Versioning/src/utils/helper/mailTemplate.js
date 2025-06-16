eea08c35ab3339e07e005cff44e52d3bc0968244
mRameez110 <temprary87@gmail.com>
2025-06-16 20:43:22 +0500

    add createdBy field to task mail template

--- Node Js/006 Api Versioning/src/utils/helper/mailTemplate.js
+++ Node Js/006 Api Versioning/src/utils/helper/mailTemplate.js
@@ -3,5 +3,7 @@ const taskMailTemplate = (title, description, createdBy, assignedTo) => `
 <h2 style="color: green;">Task Notification</h2>
 <h4><strong>Task Title:</strong> ${title}</h4>
 <p><strong>Task Description:</strong> ${description}</p>
+<p><strong>Task Created By:</strong> ${createdBy}</p>
+
 </div>
 `;
