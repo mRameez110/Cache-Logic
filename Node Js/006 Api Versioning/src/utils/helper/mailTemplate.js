91452caca6997a7ab8273071d4432d4bc10f2d6b
mRameez110 <temprary87@gmail.com>
2025-06-16 20:42:22 +0500

    add task notification heading to email template

--- Node Js/006 Api Versioning/src/utils/helper/mailTemplate.js
+++ Node Js/006 Api Versioning/src/utils/helper/mailTemplate.js
@@ -1,4 +1,6 @@
 const taskMailTemplate = (title, description, createdBy, assignedTo) => `
 <div style="font-family: Arial, sans-serif; line-height: 1.6; color: black;">
+<h2 style="color: green;">Task Notification</h2>
+
 </div>
 `;
