2538605d9391d64a2dca3bca5cc9b3e974fa22a6
mRameez110 <temprary87@gmail.com>
2025-06-17 20:48:57 +0500

    connect to MongoDB using mongoose and log result

--- Node Js/006 Api Versioning/src/utils/helper/db/connection.js
+++ Node Js/006 Api Versioning/src/utils/helper/db/connection.js
@@ -3,5 +3,11 @@ const dbURL = process.env.DB_URL;
 
 const connectDB = () => {
 	if (dbURL) {
+		mongoose
+			.connect(dbURL)
+			.then(() => console.log("Database connected successfully"))
+			.catch((err) =>
+				console.log("Something wrong in connecting Mongo DB ", err)
+			);
 	}
 };
