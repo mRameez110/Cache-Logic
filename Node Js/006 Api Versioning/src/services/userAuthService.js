606c8a250fa9792ac96499f7aa2d99dc94820d44
mRameez110 <temprary87@gmail.com>
2025-06-18 20:46:39 +0500

    fixed missing bracket error

--- Node Js/006 Api Versioning/src/services/userAuthService.js
+++ Node Js/006 Api Versioning/src/services/userAuthService.js
@@ -6,23 +6,24 @@ const {
 } = require("../utils/constants/errors/errorClass");
 
 const registerService = async (dataObject) => {
-  const { username, email, password } = dataObject;
-  const userNameAlreadyExist = await userModel.findOne({ username });
-if (userNameAlreadyExist) {
-  throw new UserAlreadyExistError("Username already in use", 400);
-}
-const userEmailAlreadyExist = await userModel.findOne({ email });
-if (userEmailAlreadyExist) {
-  throw new UserAlreadyExistError("Email already in use", 400);
-}
+	const { username, email, password } = dataObject;
+	const userNameAlreadyExist = await userModel.findOne({ username });
+	if (userNameAlreadyExist) {
+		throw new UserAlreadyExistError("Username already in use", 400);
+	}
+	const userEmailAlreadyExist = await userModel.findOne({ email });
+	if (userEmailAlreadyExist) {
+		throw new UserAlreadyExistError("Email already in use", 400);
+	}
 
-const saltRounds = 10;
-const hashPassword = await bcrypt.hash(password, saltRounds);
+	const saltRounds = 10;
+	const hashPassword = await bcrypt.hash(password, saltRounds);
 
-const registerNewUser = new userModel({
-  username,
-  email,
-  password: hashPassword,
-});
-const user = await registerNewUser.save();
-return user;
+	const registerNewUser = new userModel({
+		username,
+		email,
+		password: hashPassword,
+	});
+	const user = await registerNewUser.save();
+	return user;
+};
