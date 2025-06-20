52aa1fced716f102e7336ca3b4ee8ec10623d4ca
mRameez110 <temprary87@gmail.com>
2025-06-20 19:57:10 +0500

    get user from userModel

--- Node Js/006 Api Versioning/src/services/userAuthService.js
+++ Node Js/006 Api Versioning/src/services/userAuthService.js
@@ -36,4 +36,6 @@ const getAllUsersService = async () => {
 
 const getUserService = async (dataObject) => {
 	const userName = dataObject.params.username;
+	console.log("See data of body ", userName);
+	const user = await userModel.findOne({ username: userName });
 };
