4345080bb8654c58b12289fd384b4a388a91f743
mRameez110 <temprary87@gmail.com>
2025-06-23 21:05:36 +0500

    validate if task creator exists in the database

--- Node Js/006 Api Versioning/src/services/taskService.js
+++ Node Js/006 Api Versioning/src/services/taskService.js
@@ -4,4 +4,10 @@ const { BadRequestError } = require("../utils/constants/errors/errorClass");
 
 const createTaskService = async (dataObject) => {
 	const { title, description, createdBy, assignedTo } = dataObject;
+	const findTaskCrater = await userModel.findOne({ username: createdBy });
+	if (!findTaskCrater)
+		throw new BadRequestError(
+			"Your are not register. first register to create task",
+			400
+		);
 };
