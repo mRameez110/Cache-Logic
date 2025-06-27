f1272d6b9d94ef55687461fb4810e1b30bc6ee66
mRameez110 <temprary87@gmail.com>
2025-06-27 20:26:05 +0500

    import taskMailTemplate helper

--- Node Js/006 Api Versioning/src/services/emailService.js
+++ Node Js/006 Api Versioning/src/services/emailService.js
@@ -3,3 +3,4 @@ const fs = require("fs");
 const path = require("path");
 const handlebars = require("handlebars");
 const { MailError } = require("../utils/constants/errors/errorClass");
+const { taskMailTemplate } = require("../utils/helper/mailTemplate");
