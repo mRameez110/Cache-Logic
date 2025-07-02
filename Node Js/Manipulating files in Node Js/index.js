dc7d89f7293aa0391615870c77fbbaca70421ba6
mRameez110 <temprary87@gmail.com>
2025-07-02 20:14:52 +0500

    add setInterval to log 'Hello' every 2 seconds

--- Node Js/Manipulating files in Node Js/index.js
+++ Node Js/Manipulating files in Node Js/index.js
@@ -2,3 +2,7 @@ const timeId = setTimeout(() => {
 	console.log("Inside Timeout", timeId);
 }, 2000);
 console.log("Outside setTime ", timeId);
+
+setInterval(() => {
+	console.log("Hello");
+}, 2000);
