dc95d076cdc7d95d4a8373206073feaffa8f87ae
Rameez <114074269+mRameez110@users.noreply.github.com>
2025-07-21 23:41:56 +0500

    import fs and path modules

--- Node Js/Manipulating files in Node Js/index.js
+++ Node Js/Manipulating files in Node Js/index.js
@@ -1,17 +1,17 @@
 const timeId = setTimeout(() => {
-	console.log("Inside Timeout", timeId);
+  console.log("Inside Timeout", timeId);
 }, 2000);
 console.log("Outside setTime ", timeId);
 
 setInterval(() => {
-	console.log("Hello");
+  console.log("Hello");
 }, 2000);
 
 const EventEmitter = require("node:events");
 const eventEmitter = new EventEmitter();
 
 eventEmitter.on("start", () => {
-	console.log("This event is emitted");
+  console.log("This event is emitted");
 });
 
 eventEmitter.emit("start");
@@ -19,10 +19,15 @@ eventEmitter.emit("start");
 const fs = require("node:fs/promises");
 
 async function example() {
-	try {
-		const res = await fs.stat("./data.txt");
-		console.log(res.isFile());
-	} catch (err) {
-		console.error("Error is", err);
-	}
+  try {
+    const res = await fs.stat("./data.txt");
+    console.log(res.isFile());
+  } catch (err) {
+    console.error("Error is", err);
+  }
 }
+
+example();
+
+const fs = require("fs");
+const path = require("path");
