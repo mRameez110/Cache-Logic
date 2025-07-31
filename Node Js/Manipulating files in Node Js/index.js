18cf192c0188fc74e766e830e56636113d120673
Rameez <114074269+mRameez110@users.noreply.github.com>
2025-07-31 23:44:52 +0500

    fix minor warnig and errors

--- Node Js/Manipulating files in Node Js/index.js
+++ Node Js/Manipulating files in Node Js/index.js
@@ -4,6 +4,7 @@
 // console.log("Outside setTime ", timeId);
 
 // setInterval(() => {
+//   // runs every 2 seconds
 //   console.log("Hello");
 // }, 2000);
 
@@ -17,6 +18,13 @@
 // eventEmitter.emit("start");
 
 // const fs = require("node:fs/promises");
+
+// fs.stat("./data.txt", (err, info) => {
+//   if (err) console.log("Error in file is ", err);
+//   else console.log("Stats of the file are ", info);
+// });
+
+//  -> Change into async Await
 
 // async function example() {
 //   try {
@@ -32,67 +40,39 @@
 // const fs = require("fs");
 // const path = require("path");
 
-// console.log("Outside setTime ", timeId);
+// const info = "./data.txt";
+// console.log(path.dirname(info));
+// console.log(path.basename(info));
+// console.log(path.extname(info));
 
-// setInterval(() => {
-//   console.log("Hello");
-// }, 2000);
+// console.log(path.resolve("./data.txt")); // I will get absolute path
 
-// const EventEmitter = require("node:events");
-// const eventEmitter = new EventEmitter();
-
-// eventEmitter.on("start", () => {
-//   console.log("This event is emitted");
+// fs.open("./data.txt", "r", (err, fileDescriptor) => {
+//   console.log("File description is", fileDescriptor);
 // });
 
-// eventEmitter.emit("start");
+// let arr = [1, 2, 3, 4, 5];
 
-// const fs = require("node:fs/promises");
-// async function example() {
-//   try {
-//     const res = await fs.stat("./data.txt");
-//     console.log(res.isFile());
-//   } catch (err) {
-//     console.error("Error is", err);
-//   }
+// let arr3 = arr.forEach((ele) => {
+//   console.count(ele);
+//   return ele;
+// });
+
+// console.log(arr3);
+
+// function check() {
+//   console.trace();
 // }
-// example();
 
-const fs = require("fs");
-const path = require("path");
+// check();
 
-const info = "./data.txt";
+// function checkTime() {
+//   console.log("This is function fot execution time check");
+// }
 
-console.log(path.dirname(info));
-console.log(path.basename(info));
-console.log(path.extname(info));
-
-fs.open("./data.txt", "r", (err, fileDescriptor) => {
-  console.log("File description is", fileDescriptor);
-});
-
-let arr = [1, 2, 3, 4, 5];
-
-let arr3 = arr.forEach((ele) => {
-  console.count(ele);
-  return ele;
-});
-
-console.log(arr3);
-
-function check() {
-  console.trace();
-}
-
-check();
-
-function checkTime() {
-  console.log("This is function for execution time check");
-}
-
-console.time("checkTime()");
-checkTime();
-console.timeEnd("checkTime()");
+// console.time("checkTime()");
+// checkTime();
+// console.timeEnd("checkTime()");
 
 const color = require("chalk");
 console.log(color.yellow("Hello"));
